c8eef3d599eaa64de212d082e7f1adf099d5090f
Eric Dill <edill@bnl.gov>
2015-06-04 09:11:45 -0400

    MNT: Verified metaclass magic
    While this now seems sort of stupid, it was almost done by the
    time I figured out it was stupid to do this, so no we have a
    way to valiate that super_state_machine is working as promised.
    Even though it was working as promised to begin with. Uh, so,
    yeah. Major victory today.

--- bluesky/tests/test_state_machine.py
+++ bluesky/tests/test_state_machine.py
@@ -1,8 +1,10 @@
 """Tests for, uh, the state machine of the run engine"""
-from nose.tools import raises, assert_raises
+from nose.tools import raises, assert_raises, assert_equal
 from bluesky import RunEngineStateMachine
 from super_state_machine.errors import TransitionError
-from .utils import goto_state, define_state_machine_transitions
+from .utils import (goto_state,
+                    tautologically_define_state_machine_transitions,
+                    define_state_machine_transitions_from_class)
 
 
 sm = RunEngineStateMachine()
@@ -18,9 +20,15 @@ def _transitioner(state_machine, state_from, state_to, valid_transition):
 
 
 def test_state_machine_transitions():
-    transition_map = define_state_machine_transitions(sm)
-    for state_to, transitions in transition_map.items():
+    transition_map = tautologically_define_state_machine_transitions(sm)
+    for state_from, transitions in transition_map.items():
         # set the state correctly
-        for state_from, valid_transition in transitions:
+        for state_to, valid_transition in transitions:
             goto_state(sm, state_from)
             yield _transitioner, sm, state_from, state_to, valid_transition
+
+
+def test_super_state_machine():
+    # make sure super state machine is doing what it promises to do...
+    assert_equal(tautologically_define_state_machine_transitions(sm),
+                 define_state_machine_transitions_from_class(sm))
--- bluesky/tests/utils.py
+++ bluesky/tests/utils.py
@@ -30,17 +30,68 @@ def goto_state(state_machine, desired_state):
         state_machine.set_(state)
 
 
-def define_state_machine_transitions(state_machine):
-    all_states = set(state_machine.States.states())
-    print('all_states [[%s]]' % all_states)
-    transitions = state_machine.Meta.transitions
-    for state in all_states:
-        if state not in transitions:
-            transitions[state] = list(all_states)
-    print('transitions')
-    print(transitions)
+def tautologically_define_state_machine_transitions(state_machine):
+    """Create a mapping of all transitions in ``state_machine``
+
+    Parameters
+    ----------
+    state_machine : super_state_machine.machines.StateMachine
+        The state machine you want a complete map of
+
+    Returns
+    -------
+    dict
+        Dictionary of all transitions in ``state_machine``
+        Structured as
+        {from_state1: [(to_state, allowed), ...],
+         from_state2: [(to_state, allowed), ...],
+        }
+        where
+        - ``allowed`` is a boolean
+        - ``from_stateN`` is a string
+        - ``to_state`` is a string
+    """
+    transitions_as_enum = state_machine.__class__._meta['transitions']
+    transitions_as_names = {
+        to_state.value: [from_state.value for from_state in from_states]
+        for to_state, from_states in transitions_as_enum.items()}
     transition_map = defaultdict(list)
-    for (from_state, to_state) in product(all_states, all_states):
-        valid_transition = to_state in transitions[from_state]
-        transition_map[from_state].append((to_state, valid_transition))
+    all_states = set(state_machine.States.states())
+    for to_state, from_states in transitions_as_names.items():
+        for from_state in all_states:
+            allowed = True
+            if from_state not in from_states:
+                allowed = False
+            transition_map[to_state].append((from_state, allowed))
+
     return transition_map
+
+
+def define_state_machine_transitions_from_class(state_machine):
+    all_states = set(state_machine.States.states())
+    transitions = {from_state: set(to_states) for from_state, to_states
+                   in state_machine.Meta.transitions.items()}
+    for transition in state_machine.Meta.named_transitions:
+        try:
+            transition_name, to_state, from_states = transition
+        except ValueError:
+            transition_name, to_state = transition
+            from_states = all_states
+        for from_state in from_states:
+            transitions[from_state].add(to_state)
+    transition_map = defaultdict(list)
+    for from_state, to_states in transitions.items():
+        for to_state in all_states:
+            allowed = True
+            if to_state not in to_states:
+                allowed = False
+            transition_map[from_state].append((to_state, allowed))
+    return transition_map
+
+
+if __name__ == "__main__":
+    from bluesky import RunEngineStateMachine
+    sm = RunEngineStateMachine()
+    transition_map = dict(tautologically_define_state_machine_transitions(sm))
+    from pprint import pprint
+    pprint(transition_map)
