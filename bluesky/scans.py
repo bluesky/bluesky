2f03e2cb2288ccaa0d998f3108b382503bdffad9
danielballan <daniel.b.allan@gmail.com>
2015-06-05 12:19:32 -0400

    ENH: Add more scans.

--- bluesky/scans.py
+++ bluesky/scans.py
@@ -134,3 +134,69 @@ class LogAscan(Scan1D):
     def _gen(self):
         self._steps = np.logspace(self.start, self.stop, self.num)
         return super()._gen()
+
+
+class LinDscan(Dscan):
+    """
+    Delta (relative) scan over one variable in equally spaced steps
+
+    Parameters
+    ----------
+    motor : object
+        any 'setable' object (motor, temp controller, etc.)
+    detectors : list
+        list of 'readable' objects
+    start : float
+        starting position of motor
+    stop : float
+        ending position of motor
+    num : int
+        number of steps
+
+    Examples
+    --------
+    # Scan motor1 from 0 to 1 in ten steps.
+    >>> my_scan = LinDscan(motor1, [det1, det2], 0, 1, 10)
+    >>> RE(my_scan)
+    # Adjust a Parameter and run again.
+    >>> my_scan.num = 100
+    >>> RE(my_scan)
+    """
+    _fields = ['motor', 'detectors', 'start', 'stop', 'num']
+
+    def _gen(self):
+        self.steps = np.linspace(self.start, self.stop, self.num)
+        return super()._gen()
+
+
+class LogDscan(Dscan):
+    """
+    Delta (relative) scan over one variable in log-spaced steps
+
+    Parameters
+    ----------
+    motor : object
+        any 'setable' object (motor, temp controller, etc.)
+    detectors : list
+        list of 'readable' objects
+    start : float
+        starting position of motor
+    stop : float
+        ending position of motor
+    num : int
+        number of steps
+
+    Examples
+    --------
+    # Scan motor1 from 0 to 10 in ten log-spaced steps.
+    >>> my_scan = LogDscan(motor1, [det1, det2], 0, 1, 10)
+    >>> RE(my_scan)
+    # Adjust a Parameter and run again.
+    >>> my_scan.num = 100
+    >>> RE(my_scan)
+    """
+    _fields = ['motor', 'detectors', 'start', 'stop', 'num']
+
+    def _gen(self):
+        self.steps = np.logspace(self.start, self.stop, self.num)
+        return super()._gen()
