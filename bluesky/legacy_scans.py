87d62b25a6bc16fa23696a05d3760b00ddfa311a
danielballan <daniel.b.allan@gmail.com>
2015-06-05 16:06:43 -0400

    FIX: Deprecation message had 'ascan' hard-coded.

--- bluesky/legacy_scans.py
+++ bluesky/legacy_scans.py
@@ -7,9 +7,10 @@ All of these are deprecated. See bluesky/scans.py for new versions.
 import warnings
 from .scans import LinDscan, LinAscan, Count
 
-def _deprecation_warning(replacement):
-    warnings.warn("ascan is deprecated, and it does not provide "
-                  "the latest functionality. Use %s instead." % replacement,
+def _deprecation_warning(old, new):
+    warnings.warn("{old} is deprecated, and it does not provide "
+                  "the latest functionality. Use {new} instead.".format(
+                      old=old, new=new),
                   UserWarning)
 
 
@@ -43,14 +44,14 @@ class _LegacyScan:
 
 class LegacyAscan(_LegacyScan):
     def __call__(self, motor, start, stop, num):
-        _deprecation_warning('LinAscan')
+        _deprecation_warning('ascan', 'LinAscan')
         curried = lambda dets: LinAscan(motor, dets, start, stop, num)
         super()._run(curried)
 
 
 class LegacyDscan(_LegacyScan):
     def __call__(self, motor, start, stop, num):
-        _deprecation_warning('LinDscan')
+        _deprecation_warning('dscan', 'LinDscan')
         dets = set(self.detectors) | set(self.default_detectors)
         curried = lambda dets: LinDscan(motor, dets, start, stop, num)
         super()._run(curried)
@@ -58,6 +59,6 @@ class LegacyDscan(_LegacyScan):
 
 class LegacyCount(_LegacyScan):
     def __call__(self):
-        _deprecation_warning('Count')
+        _deprecation_warning('ct', 'Count')
         curried = lambda dets: Count(dets)
         super()._run(curried)
