1304abac3f17471581ee74908bf945d204d02009
Thomas A Caswell <tcaswell@bnl.gov>
2015-06-08 14:22:10 -0400

    ENH: add some more detectors

--- bluesky/examples.py
+++ bluesky/examples.py
@@ -205,6 +205,9 @@ motor1 = Mover('motor1', ['motor1'])
 motor2 = Mover('motor2', ['motor2'])
 motor3 = Mover('motor3', ['motor3'])
 det = SynGauss('det', motor, 'motor', center=0, Imax=1, sigma=1)
+det1 = SynGauss('det1', motor1, 'motor1', center=0, Imax=5, sigma=0.5)
+det2 = SynGauss('det2', motor2, 'motor2', center=1, Imax=2, sigma=2)
+det3 = SynGauss('det3', motor3, 'motor3', center=-1, Imax=2, sigma=1)
 
 
 def simple_scan(motor):
