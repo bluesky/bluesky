840d425ad9479571ccf09412cce96e1df9f33b14
Eric Dill <thedizzle@gmail.com>
2015-06-10 11:02:04 -0400

    ENH: Actually make the image callback work

--- examples/plotting_2d.py
+++ examples/plotting_2d.py
@@ -9,34 +9,6 @@ import time as ttime
 from filestore.handlers import NpyHandler
 fsapi.register_handler('npy', NpyHandler)
 
-class ImageCallback(CallbackBase):
-
-    def __init__(self, datakey, fig=None):
-        # wheeee MRO
-        super(ImageCallback, self).__init__()
-        if fig is None:
-            fig = plt.figure()
-        self.cs = CrossSection(fig)
-        self.datakey = datakey
-        show_all_figures()
-
-    def event(self, doc):
-        uid = doc['data'][self.datakey]
-        data = fsapi.retrieve(uid)
-        self.cs.update_image(data)
-        draw_all_figures()
-
-
-def show_all_figures():
-    for f_mgr in plt._pylab_helpers.Gcf.get_all_fig_managers():
-        f_mgr.canvas.figure.show()
-
-
-def draw_all_figures():
-    for f_mgr in plt._pylab_helpers.Gcf.get_all_fig_managers():
-        f_mgr.canvas.draw()
-        f_mgr.canvas.flush_events()
-
 
 def stepscan(motor, det):
     for i in np.linspace(-5, 5, 75):
