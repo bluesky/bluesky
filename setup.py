36247d04a5e8ca8dd2a7da82df1f1a28d6db035f
Thomas A Caswell <tcaswell@gmail.com>
2015-06-09 21:31:15 -0400

    MNT: simplify setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from __future__ import (absolute_import, division, print_function)
-import sys
-import warnings
-import shutil
+
 try:
     from setuptools import setup
 except ImportError:
@@ -10,68 +8,14 @@ except ImportError:
     except ImportError:
         from distutils.core import setup
 
-from distutils.core import setup, Extension
-import os
-
-MAJOR = 0
-MINOR = 0
-MICRO = 0
-ISRELEASED = False
-SNAPSHOT = False
-VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
-QUALIFIER = ''
-
-FULLVERSION = VERSION
-print(FULLVERSION)
-
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-if not ISRELEASED:
-    import subprocess
-    FULLVERSION += '.post0'
-    if SNAPSHOT:
-        pipe = None
-        for cmd in ['git', 'git.cmd']:
-            try:
-                pipe = subprocess.Popen([cmd, "describe", "--always",
-                                         "--match", "v[0-9\/]*"],
-                                        stdout=subprocess.PIPE)
-                (so, serr) = pipe.communicate()
-                print(so, serr)
-                if pipe.returncode == 0:
-                    pass
-                print('here')
-            except:
-                pass
-            if pipe is None or pipe.returncode != 0:
-                warnings.warn("WARNING: Couldn't get git revision, "
-                              "using generic version string")
-            else:
-                rev = so.strip()
-                # makes distutils blow up on Python 2.7
-                if sys.version_info[0] >= 3:
-                    rev = rev.decode('ascii')
-
-                # use result of git describe as version string
-                FULLVERSION = VERSION + '-' + rev.lstrip('v')
-                break
-else:
-    FULLVERSION += QUALIFIER
-
 setup(
     name='bluesky',
-    version=FULLVERSION,
+    version='0.0.0.post1',
     author='danielballan',
     author_email=None,
     license="BSD (3-clause)",
     url="https://github.com/danielballan/bluesky",
     packages=['bluesky'],
-    long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3.4",
