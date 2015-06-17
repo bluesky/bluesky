3a5a37980cc513e3c5e7c138845e5839b6acbb01
Eric Dill <thedizzle@gmail.com>
2015-06-16 21:12:15 -0400

    ENH: Make this a "validate all events" function

--- bluesky/broker_callbacks.py
+++ bluesky/broker_callbacks.py
@@ -4,15 +4,24 @@ See also bluesky/callbacks.py
 Callbacks that require DataBroker are in this separate file to isolate the
 dependency.
 """
-
+import sys
+import traceback
 from dataportal import DataBroker as db
 
 
-def filestore_validation(run_stop):
+def validate_all_events(run_stop):
     uid = run_stop['run_start_uid']
     hdr = db[uid]
     events = db.fetch_events(hdr)
     # if there are filestore references in the events then this will attempt
     # to dereference all of them and if it cannot find an entry, should raise
     # from filestore.
-    list(events)
+    while True:
+        try:
+            e = next(events)
+        except Exception as e:
+            print("Accessing event (%d) %s raised the following Exception" %
+                  (e.seq_num, e.uid))
+            tb_list = traceback.extract_tb(sys.exc_info()[2])
+            for item in traceback._format_list_iter(tb_list):
+                print('\t%s' % item)
