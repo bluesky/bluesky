6c50f32ce469494fafe8c7144f0b649a7373c226
Dan Allan <dallan@bnl.gov>
2015-07-02 01:16:04 -0400

    ENH: Add hardware checklist functions with tests.

---
+++ bluesky/tests/test_hardware_checklist.py
@@ -0,0 +1,58 @@
+import nose
+from nose.tools import assert_raises
+from bluesky.hardware_checklist import *
+
+
+def test_connect_mds_mongodb():
+    try:
+        import metadatastore
+    except ImportError:
+        raise nose.SkipTest
+    from metadatastore.utils.testing import mds_setup, mds_teardown
+    try:
+        mds_setup()
+        connect_mds_mongodb()
+    except:
+        raise
+    finally:
+        mds_teardown()
+
+
+def test_connect_fs_mongodb():
+    try:
+        import filestore
+    except ImportError:
+        raise nose.SkipTest
+    from filestore.utils.testing import fs_setup, fs_teardown
+    try:
+        fs_setup()
+        connect_fs_mongodb()
+    except:
+        raise
+    finally:
+        fs_teardown()
+
+
+def test_check_storage():
+    check_storage('/', 1)
+    assert_raises(RuntimeError, check_storage, '/', 10000000000000000000)
+
+
+def test_connect_channelarchiver():
+    # Just test failure, not success.
+    assert_raises(RuntimeError, connect_channelarchiver, 'http://a.b.c')
+
+
+def test_connect_pv():
+    try:
+        import epics
+    except ImportError:
+        raise nose.SkipTest()
+    pv_name = 'BSTEST:VAL'
+    connect_pv(pv_name)
+    epics.caput(pv_name, 5, wait=True)
+    assert_pv_equal(pv_name, 5)
+    assert_pv_greater(pv_name, 4)
+    assert_pv_less(pv_name, 6)
+    assert_pv_in_band(pv_name, 4, 6)
+    assert_pv_out_of_band(pv_name, 2, 4)
