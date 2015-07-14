a1e921fe21ecedfbe8906a4f5b1c7bf79bcaf805
danielballan <daniel.b.allan@gmail.com>
2015-07-14 16:43:18 -0400

    FIX: Make drawing compatible with mpl < 1.5.x.

--- bluesky/callbacks.py
+++ bluesky/callbacks.py
@@ -21,11 +21,22 @@ logger = logging.getLogger(__name__)
 _create_qApp()
 qApp = matplotlib.backends.backend_qt5.qApp
 
+from matplotlib._pylab_helpers import Gcf
+
+try:
+    _draw_all = Gcf.draw_all  # mpl version >= 1.5
+except AttributeError:
+    # slower, but backward-compatible
+    def _draw_all():
+        for f_mgr in Gcf.get_all_fig_managers():
+            f_mgr.canvas.draw_idle()
+
 
 def _qt_kicker():
     # The RunEngine Event Loop interferes with the qt event loop. Here we
     # kick it to keep it going.
-    plt.draw_all()
+    _draw_all()
+
     qApp.processEvents()
     loop.call_later(0.1, _qt_kicker)
 
