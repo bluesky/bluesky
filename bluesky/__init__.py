8d056da5cd93aec6645cf0c41236599cc2b85edd
danielballan <daniel.b.allan@gmail.com>
2015-08-17 17:56:24 -0400

    BLD: Remove hard-coded __version__

--- bluesky/__init__.py
+++ bluesky/__init__.py
@@ -3,7 +3,6 @@ from .run_engine import (Msg, RunEngine, PanicError, RunInterrupt)
 from .scans import *
 
 logger = logging.getLogger(__name__)
-__version__ = '0.1.0'
 
 from ._version import get_versions
 __version__ = get_versions()['version']
