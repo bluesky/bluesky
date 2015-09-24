MNT: workaround off-by-one bug in sequence number

bluesky/callbacks.py
@@ -521,8 +521,9 @@ class LiveRaster(CallbackBase):
         cb.set_label('I')
 
     def event(self, doc):
-        seq_num = doc['seq_num']
+        seq_num = doc['seq_num'] - 1
         pos = np.unravel_index(seq_num, self.raster_shape)
+
         self._Idata[pos] = doc['data'][self.I]
 
         self.im.set_array(self._Idata)

examples/basic_raster_plot.py
@@ -14,6 +14,6 @@ cy = ((cycler('x', np.linspace(0, 1, xct, endpoint=True)) *
        cycler('y', np.linspace(0, 1, yct, endpoint=True))) +
       cycler('I', np.linspace(0, 1, ict, endpoint=True)))
 for j, d in enumerate(cy):
-    ev = {'data': d, 'seq_num': j}
+    ev = {'data': d, 'seq_num': j + 1}
     cb('event', ev)
     plt.pause(.1)