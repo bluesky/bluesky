4f192b7dcdf23cd885692402deb722f0645210a9
Thomas A Caswell <tcaswell@gmail.com>
2015-11-06 23:37:39 -0500

    CLN/API: remove unused class

--- bluesky/utils.py
+++ bluesky/utils.py
@@ -290,35 +290,6 @@ class Struct(metaclass=StructMeta):
         self.flyers = []
 
 
-class ExtendedList(list):
-    "A list with some 'required' elements that can't be removed."
-    # Elaborated version of http://stackoverflow.com/a/16380637/1221924
-    def __init__(self, other=None):
-        self.other = other or []
-
-    def __len__(self):
-        return list.__len__(self) + len(self.other)
-
-    def __iter__(self):
-        return itertools.chain(list.__iter__(self), iter(self.other))
-
-    def __getitem__(self, index):
-        l = list.__len__(self)
-
-        if index > l:
-            return self.other[index - l]
-        else:
-            return list.__getitem__(self, index)
-
-    def __contains__(self, value):
-        return super().__contains__(value) or (value in self.other)
-
-    def remove(self, value):
-        if (not super().__contains__(value)) and (value in self):
-            raise ValueError('%s is mandatory and cannot be removed' % value)
-        super().remove(value)
-
-
 SUBS_NAMES = ['all', 'start', 'stop', 'event', 'descriptor']
 
 
