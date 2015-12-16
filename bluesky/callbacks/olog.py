925c6a53eea4b8ca4e3243a5a651873efb72ef4d
Thomas A Caswell <tcaswell@bnl.gov>
2015-12-16 09:32:47 -0500

    ENH: use jinja2 templating for olog message
     - make better use of the text / attachment fields in olog
     - use properties in a way that will not blow up (hopefully?)
     - text configurable via a jinja template for both the description
       and attached text file.

--- bluesky/callbacks/olog.py
+++ bluesky/callbacks/olog.py
@@ -1,58 +1,122 @@
-def log(name, doc):
-    # This only applies to 'start' Documents.
-    if doc != 'start':
-        return
-    input_message, = msg.args
-    output = """
-Header uid: {{uid!r}}
+from io import StringIO
+TEMPLATES = {}
+TEMPLATES['long'] = """
+{{- start.plan_type }} ['{{ start.uid[:6] }}'] (scan num: {{ start.scan_id }})
 
 Scan Plan
 ---------
-{input_message}
+{{ start.plan_type }}
+{%- for k, v in start.plan_args.items() %}
+    {{ k }}: {{ v }}
+{%-  endfor %}
 
-Metadata
---------
-{{metadata!r}}
-""".format(input_message=t
+{% if 'signature' in start -%}
+Call:
+    {{ start.signature }}
+{% endif %}
+Metaadata
+---------
+{% for k, v in start.items() -%}
+{%- if k not in ['plan_type', 'plan_args'] -%}{{ k }} : {{ v }}
+{% endif -%}
+{%- endfor -%}"""
+TEMPLATES['desc'] = """
+{{- start.plan_type }} ['{{ start.uid[:6] }}'] (scan num: {{ start.scan_id }})"""
+TEMPLATES['call'] = """RE({{ start.plan_type }}(
+{%- for k, v in start.plan_args.items() %}{%- if not loop.first %}   {% endif %}{{ k }}={{ v }}
+{%- if not loop.last %},
+{% endif %}{% endfor %}))
+"""
 
-    d = doc
-    self.logbook(log_message, d)
+def logbook_cb_factory(logbook_func, desc_template=None, long_template=None):
+    """Create a logbook run_start callback
 
+    The returned function is suitable for registering as
+    a 'start' callback on the the BlueSky run engine.
 
-def _run_engine_log_template(metadata):
-    template = []
-    for key in metadata:
-        template.append("{key}: {{{key}}}".format(key=key))
-    return '\n'.join(template)
+    Parameters
+    ----------
 
+    logbook_func : callable
+        The required signature is ::
 
-def _logmsg(md):
+            def logbok_func(text=None, logbooks=None, tags=None, properties=None,
+                            attachments=None, verify=True, ensure=False):
+                '''
 
-    call_str = _call_str(md['plan_type'], md['plan_args'])
+                Parameters
+                ----------
+                text : string
+                    The body of the log entry.
+                logbooks : string or list of strings
+                    The logbooks which to add the log entry to.
+                tags : string or list of strings
+                    The tags to add to the log entry.
+                properties : dict of property dicts
+                    The properties to add to the log entry
+                attachments : list of file like objects
+                    The attachments to add to the log entry
+                verify : bool
+                    Check that properties, tags and logbooks are in the Olog
+                    instance.
+                ensure : bool
+                    If a property, tag or logbook is not in the Olog then
+                    create the property, tag or logbook before making the log
+s                    entry. Seting ensure to True will set verify to False.
 
-    msgs = ['Scan Class: {scn_cls}', '']
-    for k, v in md['plan_args'].items():
-        msgs.append('{k}: {{{k}!r}}'.format(k=k))
-    msgs.append('')
-    msgs.append('To call:')
-    msgs.extend(call_str)
-    return msgs
+                '''
+                pass
 
+        This matches the API on `SimpleOlogClient.log`
 
-def logmsg(self):
-    msgs = self._logmsg()
-    return '\n'.join(msgs)
+    """
+    import jinja2
+    env = jinja2.Environment()
+    if long_template is None:
+        long_template = TEMPLATES['long']
+    if desc_template is None:
+        desc_template = TEMPLATES['desc']
+    # It seems that the olog only has one text field, which it calls
+    # `text` on the python side and 'description' on the olog side.
+    # There are some CSS applications that try to shove the entire
+    # thing into a single line.  We work around this by doing two
+    # strings, a long one which will get put in a as an attachment
+    # and a short one to go in as the 'text' which will be used as the
+    # description
+    long_msg = env.from_string(long_template)
+    desc_msg = env.from_string(desc_template)
 
+    def lbcb(name, doc):
+        # This only applies to 'start' Documents.
+        if name != 'start':
+            return
 
-def logdict(self):
-    out_dict = {k: getattr(self, k) for k in self._fields}
-    out_dict['scn_cls'] = self.__class__.__name__
-    return out_dict
+        atch = StringIO(long_msg.render(start=doc))
+        desc = desc_msg.render(start=doc)
+        logbook_func(text=desc, properties={'start':doc},
+                attachments=[atch],
+                ensure=True)
+    return lbcb
 
+def call_str(start, call_template=None):
+    """Given a start document generate an evalable call scring
 
-def _call_str(plan_type, plan_args):
-    args = []
-    for k, v in plan_args.items():
-        args.append("{k}={{{k}!r}}".format(k=k))
+    The default template assumes that `plan_args` and `plan_type`
+    are at the top level of the document.
 
-    return ["RE({{scn_cls}}({args}))".format(args=', '.join(args)), ]
+    Parameter
+    ---------
+    start : dict
+        A document which follows the runstart schema
+
+    call_template : str, optional
+        A jinja2 template rendered with `cr.render(start=start)`
+
+        If not provided defaults to `CALL_TEMPLATE`
+    """
+    import jinja2
+    env = jinja2.Environment()
+    if call_template is None:
+        call_template = TEMPLATES['call']
+    call_renderer = env.from_string(call_template)
+    return call_renderer.render(start=start)
