55f4e9aa830aed7ffe2b188f2d33d40b677fa651
Thomas A Caswell <tcaswell@bnl.gov>
2016-01-19 00:01:39 -0500

    MNT: custom is no longer needed in MDS api

--- bluesky/register_mds.py
+++ bluesky/register_mds.py
@@ -16,43 +16,6 @@ def _make_insert_func(func):
     return inserter
 
 
-known_run_start_keys = ['time', 'scan_id', 'beamline_id', 'uid', 'owner',
-                        'group', 'project']
-
-
-def _insert_run_start(name, doc):
-    """Rearrange the dict for unpacking it into the MDS API."""
-    # Move dynamic keys into 'custom' for MDS API.
-    # We should change this in MDS to save the time of copying here:
-    doc = copy.deepcopy(doc)
-    for key in list(doc):
-        if key not in known_run_start_keys:
-            try:
-                doc['custom']
-            except KeyError:
-                doc['custom'] = {}
-            doc['custom'][key] = doc.pop(key)
-    return mds.insert_run_start(**doc)
-
-
-known_descriptor_keys = ['run_start', 'data_keys', 'time', 'uid']
-
-
-def _insert_descriptor(name, doc):
-    """Rearrange the dict for unpacking it into the MDS API."""
-    # Move dynamic keys into 'custom' for MDS API.
-    # We should change this in MDS to save the time of copying here:
-    doc = copy.deepcopy(doc)
-    for key in list(doc):
-        if key not in known_descriptor_keys:
-            try:
-                doc['custom']
-            except KeyError:
-                doc['custom'] = {}
-            doc['custom'][key] = doc.pop(key)
-    return mds.insert_descriptor(**doc)
-
-
 def _insert_bulk_events(name, doc):
     """Bulk insert each event stream in doc."""
     for desc_uid, events in doc.items():
@@ -62,8 +25,9 @@ def _insert_bulk_events(name, doc):
 
 insert_funcs = {DocumentNames.event: _make_insert_func(mds.insert_event),
                 DocumentNames.bulk_events: _insert_bulk_events,
-                DocumentNames.descriptor: _insert_descriptor,  # see above
-                DocumentNames.start: _insert_run_start,  # see above
+                DocumentNames.descriptor:
+                    _make_insert_func(mds.insert_descriptor),
+                DocumentNames.start: _make_insert_func(mds.insert_run_start),
                 DocumentNames.stop: _make_insert_func(mds.insert_run_stop)}
 
 
