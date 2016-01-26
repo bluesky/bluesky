e56ef97f80321e1d83b623b3488189441d48b490
Eric Dill <edill@bnl.gov>
2016-01-26 17:33:56 -0500

    TST: Switch vertical integration to pytest

--- bluesky/tests/test_vertical_integration.py
+++ bluesky/tests/test_vertical_integration.py
@@ -1,9 +1,9 @@
 from collections import defaultdict
-from nose import SkipTest
-from nose.tools import assert_equal
 from bluesky.examples import stepscan, det, motor
+import pytest
 
-def setup():
+
+def setup_module():
     try:
         from metadatastore.utils.testing import mds_setup
     except ImportError:
@@ -11,7 +11,7 @@ def setup():
     else:
         mds_setup()
 
-def teardown():
+def teardown_module():
     try:
         from metadatastore.utils.testing import mds_teardown
     except ImportError:
@@ -24,12 +24,12 @@ def test_scan_and_get_data():
     try:
         import metadatastore
         del metadatastore
-    except ImportError:
-        raise SkipTest
+    except ImportError as ie:
+        raise pytest.skip('skipping because metadatastore is not available\nMessage is: {}'.format(ie))
     try:
         from databroker import DataBroker as db
-    except ImportError:
-        raise SkipTest
+    except ImportError as ie:
+        raise pytest.skip('skipping because databroker is not available\nMessage is:{}'.format(ie))
     from bluesky.standard_config import gs
     uid = gs.RE(stepscan(det, motor), group='foo', beamline_id='testing',
              config={})
@@ -42,8 +42,8 @@ def test_post_run():
     try:
         import databroker
         del databroker
-    except ImportError:
-        raise SkipTest('requires databroker')
+    except ImportError as ie:
+        raise pytest.skip('skipping because databroker is not available\nMessage is:{}'.format(ie))
     from bluesky.standard_config import gs
     from bluesky.broker_callbacks import post_run
     output = defaultdict(list)
@@ -54,11 +54,7 @@ def test_post_run():
 
     gs.RE(stepscan(det, motor), subs={'stop': [post_run(do_nothing)]})
     assert len(output)
-    assert_equal(len(output['start']), 1)
-    assert_equal(len(output['stop']), 1)
-    assert_equal(len(output['descriptor']), 1)
-    assert_equal(len(output['event']), 10)
-
-if __name__ == '__main__':
-    import nose
-    nose.runmodule(argv=['-s', '--with-doctest'], exit=False)
+    assert len(output['start']) == 1
+    assert len(output['stop']) == 1
+    assert len(output['descriptor']) == 1
+    assert len(output['event']) == 10
