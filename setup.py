ca00ae331dfa85883bb7592755b7ca1f6b332404
Eric Dill <edill@bnl.gov>
2016-01-26 18:01:31 -0500

    Rework setuptools

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
 from __future__ import (absolute_import, division, print_function)
 import versioneer
 
-try:
-    from setuptools import setup
-except ImportError:
-    try:
-        from setuptools.core import setup
-    except ImportError:
-        from distutils.core import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name='bluesky',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
@@ -17,8 +11,7 @@ setup(
     author_email=None,
     license="BSD (3-clause)",
     url="https://github.com/danielballan/bluesky",
-    packages=['bluesky', 'bluesky.tests', 'bluesky.testing',
-              'bluesky.callbacks'],
+    packages=setuptools.find_packages(),
     package_data={'bluesky': ['schema/*.json']},
     install_requires=['jsonschema', 'traitlets', 'prettytable', 'cycler',
                       'numpy', 'matplotlib', 'super_state_machine',
