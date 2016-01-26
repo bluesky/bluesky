1e2e6c01f2876d66f56bef0e05edd1f563d1f31b
Eric Dill <edill@bnl.gov>
2016-01-25 19:55:03 -0500

    TST: Switch test_examples to pytest

--- bluesky/tests/test_examples.py
+++ bluesky/tests/test_examples.py
@@ -1,7 +1,4 @@
-import nose
-from nose import SkipTest
-from nose.tools import (assert_equal, assert_is, assert_is_none, assert_raises,
-                        assert_true, assert_in, assert_not_in)
+import pytest
 from bluesky.examples import (motor, simple_scan, det, sleepy, wait_one,
                               wait_multiple, motor1, motor2, conditional_pause,
                               loop, checkpoint_forever, simple_scan_saving,
@@ -11,7 +8,6 @@ from bluesky.examples import (motor, simple_scan, det, sleepy, wait_one,
 from bluesky.callbacks import LivePlot
 from bluesky import RunEngine, Msg, PanicError, IllegalMessageSequence
 from bluesky.tests.utils import setup_test_run_engine
-from bluesky.testing.noseclasses import KnownFailureTest
 import os
 import signal
 import asyncio
@@ -25,40 +21,43 @@ except ImportError:
 else:
     skip_mpl = False
 
+try:
+    import historydict
+except ImportError:
+    historydict = None
 
 RE = setup_test_run_engine()
 
 
 def test_msgs():
     m = Msg('set', motor, {'motor': 5})
-    assert_equal(m.command, 'set')
-    assert_is(m.obj, motor)
-    assert_equal(m.args, ({'motor': 5},))
-    assert_equal(m.kwargs, {})
+    assert m.command == 'set'
+    assert m.obj is motor
+    assert m.args == ({'motor': 5},)
+    assert m.kwargs == {}
 
     m = Msg('read', motor)
-    assert_equal(m.command, 'read')
-    assert_is(m.obj, motor)
-    assert_equal(m.args, ())
-    assert_equal(m.kwargs, {})
+    assert m.command == 'read'
+    assert m.obj is motor
+    assert m.args == tuple()
+    assert m.kwargs == {}
 
     m = Msg('create')
-    assert_equal(m.command, 'create')
-    assert_is_none(m.obj)
-    assert_equal(m.args, ())
-    assert_equal(m.kwargs, {})
+    assert m.command == 'create'
+    assert m.obj is None
+    assert m.args == tuple()
+    assert m.kwargs == {}
 
     m = Msg('sleep', None, 5)
-    assert_equal(m.command, 'sleep')
-    assert_is_none(m.obj)
-    assert_equal(m.args, (5,))
-    assert_equal(m.kwargs, {})
-
+    assert m.command == 'sleep'
+    assert m.obj is None
+    assert m.args == (5,)
+    assert m.kwargs == {}
 
 def run(gen, *args, **kwargs):
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
     RE(gen(*args, **kwargs))
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
 
 
 def test_simple():
@@ -82,79 +81,81 @@ def test_wait_multiple():
 
 
 def test_hard_pause():
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
     RE(conditional_pause(det, motor, False, True))
-    assert_equal(RE.state, 'paused')
+    assert RE.state == 'paused'
     RE.resume()
-    assert_equal(RE.state, 'paused')
+    assert RE.state == 'paused'
     RE.abort()
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
 
 
 def test_deferred_pause():
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
     RE(conditional_pause(det, motor, True, True))
-    assert_equal(RE.state, 'paused')
+    assert RE.state == 'paused'
     RE.resume()
-    assert_equal(RE.state, 'paused')
+    assert RE.state == 'paused'
     RE.abort()
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
 
 
 def test_hard_pause_no_checkpoint():
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
     RE(conditional_pause(det, motor, False, False))
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
 
 
 def test_deferred_pause_no_checkpoint():
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
     RE(conditional_pause(det, motor, True, False))
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
 
 
 def test_pause_from_outside():
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
 
     def local_pause():
         RE.request_pause()
 
     loop.call_later(1, local_pause)
     RE(checkpoint_forever())
-    assert_equal(RE.state, 'paused')
+    assert RE.state == 'paused'
 
     # Cue up a second pause requests in 2 seconds.
     loop.call_later(2, local_pause)
     RE.resume()
-    assert_equal(RE.state, 'paused')
+    assert RE.state == 'paused'
 
     RE.abort()
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
 
 
 def test_panic_during_pause():
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
     RE(conditional_pause(det, motor, False, True))
     RE.panic()
-    assert_true(RE._panic)
-    assert_raises(PanicError, lambda: RE.resume())
+    assert RE._panic
+    with pytest.raises(PanicError):
+        RE.resume()
     # If we panic while paused, we can un-panic and resume.
     RE.all_is_well()
-    assert_equal(RE.state, 'paused')
+    assert RE.state == 'paused'
     RE.abort()
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
 
 
 def test_panic_timer():
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
     panic_timer(RE, 1)  # panic in 1 second
-    assert_raises(PanicError, RE, checkpoint_forever())
+    with pytest.raises(PanicError):
+        RE(checkpoint_forever())
     # If we panic while runnning, we cannot resume. The run is aborted and we
     # land in 'idle'
-    assert_equal(RE.state, 'idle')
-    assert_true(RE._panic)
+    assert RE.state == 'idle'
+    assert RE._panic
     RE.all_is_well()
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
 
 
 def test_simple_scan_saving():
@@ -166,58 +167,58 @@ def print_event_time(name, doc):
 
 
 def test_calltime_subscription():
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
     RE(simple_scan_saving(det, motor), subs={'event': print_event_time})
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
 
 
 def test_stateful_subscription():
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
     token = RE.subscribe('event', print_event_time)
     RE(simple_scan_saving(det, motor))
     RE.unsubscribe(token)
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
 
-
+pytest.mark.skip_if(skip_mpl)
 def test_live_plotter():
-    if skip_mpl:
-        raise nose.SkipTest("matplotlib is not available")
     my_plotter = LivePlot('det', 'motor')
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
     RE(stepscan(det, motor), subs={'all': my_plotter})
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
 
 
 def test_sample_md_dict_requirement():
     # We avoid a json ValidationError and make a user-friendly ValueError.
-    assert_raises(ValueError, RE, simple_scan(motor), sample=1)
+    with pytest.raises(ValueError):
+        RE(simple_scan(motor), sample=1)
     RE(simple_scan(motor), sample={'number': 1})  # should not raise
 
 
 def test_md_dict():
     yield _md, {}
 
-
+@pytest.mark.skip_if(historydict is None)
 def test_md_historydict():
-    try:
-        from historydict import HistoryDict
-    except ImportError:
-        raise SkipTest()
-    yield _md, HistoryDict(':memory:')
+    yield _md, historydict.HistoryDict(':memory:')
 
 
 def _md(md):
     RE = RunEngine(md)
     RE.ignore_callback_exceptions = False
     scan = simple_scan(motor)
-    assert_raises(KeyError, RE, scan)  # missing owner, beamline_id
+    with pytest.raises(KeyError):
+        RE(scan)  # missing owner, beamline_id
     scan = simple_scan(motor)
-    assert_raises(KeyError, RE, scan, owner='dan')
+    with pytest.raises(KeyError):
+        RE(scan, owner='dan')
     scan = simple_scan(motor)
     RE(scan, owner='dan', beamline_id='his desk',
        group='some group', config={})  # this should work
     scan = simple_scan(motor)
-    assert_raises(KeyError, RE, scan)  # this should fail; none was persisted
+    with pytest.raises(KeyError):
+        # TODO: Is this persistent thing still true?  I thought that something
+        # substantial changed recently. -ericdill
+        RE(scan)  # this should fail; none was persisted
     RE.md['owner'] = 'dan'
     RE.md['group'] = 'some group'
     RE.md['config'] = {}
@@ -226,7 +227,8 @@ def _md(md):
     RE(scan)  # this should work
     RE.md.clear()
     scan = simple_scan(motor)
-    assert_raises(KeyError, RE, scan)
+    with pytest.raises(KeyError):
+        RE(scan)
     # We can prime the md directly.
     RE.md['owner'] = 'dan'
     RE.md['group'] = 'some group'
@@ -250,19 +252,19 @@ def _md(md):
     RE(scan, project='standing',
        subs={'start': [validate_dict_cb('project', 'standing')]})
     # ...but they do not update the value in md
-    assert_equal(RE.md['project'], 'sitting')
+    assert RE.md['project'] == 'sitting'
 
 
 def validate_dict_cb(key, val):
     def callback(name, doc):
-        assert_in(key, doc)
-        assert_equal(doc[key], val)
+        assert key in doc
+        assert doc[key] == val
     return callback
 
 
 def validate_dict_cb_opposite(key):
     def callback(name, doc):
-        assert_not_in(key, doc)
+        assert key not in doc
     return callback
 
 
@@ -292,7 +294,7 @@ def test_suspend():
         Msg('save'),
         Msg('close_run'),
     ]
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
 
     def local_suspend():
         RE.request_suspend(ev.wait())
@@ -315,7 +317,7 @@ def test_suspend():
     # check to make sure it took long enough
     assert out[0]['time'] - start > 1.1
 
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
 
 
 def test_pause_resume():
@@ -331,16 +333,16 @@ def test_pause_resume():
         os.kill(pid, signal.SIGINT)
 
     scan = [Msg('checkpoint'), Msg('wait_for', [ev.wait(), ]), ]
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
     start = ttime.time()
     loop.call_later(1, sim_kill)
     loop.call_later(2, done)
 
     RE(scan)
-    assert_equal(RE.state, 'paused')
+    assert RE.state == 'paused'
     mid = ttime.time()
     RE.resume()
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
     stop = ttime.time()
 
     assert mid - start > 1
@@ -360,24 +362,23 @@ def test_pause_abort():
         os.kill(pid, signal.SIGINT)
 
     scan = [Msg('checkpoint'), Msg('wait_for', [ev.wait(), ]), ]
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
     start = ttime.time()
     loop.call_later(1, sim_kill)
     loop.call_later(2, done)
 
     RE(scan)
-    assert_equal(RE.state, 'paused')
+    assert RE.state == 'paused'
     mid = ttime.time()
     RE.abort()
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
     stop = ttime.time()
 
     assert mid - start > 1
     assert stop - start < 2
 
-
+@pytest.mark.known_fail
 def test_abort():
-    raise KnownFailureTest
     ev = asyncio.Event()
 
     def done():
@@ -393,13 +394,13 @@ def test_abort():
 
     scan = [Msg('checkpoint'), Msg('wait_for', [ev.wait(), ]), ]
     RE.verbose = True
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
     start = ttime.time()
     # loop.call_later(1, sim_kill)
     # loop.call_later(2, done)
 
     RE(scan)
-    assert_equal(RE.state, 'idle')
+    assert RE.state == 'idle'
     stop = ttime.time()
 
     assert stop - start < 2
@@ -412,7 +413,7 @@ def test_rogue_sigint():
         raise KeyboardInterrupt
 
     RE(bad_scan())
-    assert_equal(RE.state, 'paused')
+    assert RE.state == 'paused'
     RE.abort()
 
 
@@ -444,7 +445,7 @@ def test_seqnum_nonrepeated():
     RE(gen(), {'event': f})
     print("RESUMING!!!!")
     RE.resume()
-    assert_equal(seq_nums, [1, 2, 2, 3])
+    assert seq_nums == [1, 2, 2, 3]
 
 
 def test_duplicate_keys():
@@ -460,7 +461,7 @@ def test_duplicate_keys():
         yield(Msg('read', det2))
         yield(Msg('save'))
 
-    with assert_raises(ValueError):
+    with pytest.raises(ValueError):
         RE(gen())
 
 
@@ -473,7 +474,7 @@ def test_illegal_sequences():
         yield(Msg('create'))
         yield(Msg('close_run'))
 
-    with assert_raises(IllegalMessageSequence):
+    with pytest.raises(IllegalMessageSequence):
         RE(gen1())
 
     def gen2():
@@ -484,7 +485,7 @@ def test_illegal_sequences():
         yield(Msg('save'))
         yield(Msg('close_run'))
 
-    with assert_raises(IllegalMessageSequence):
+    with pytest.raises(IllegalMessageSequence):
         RE(gen1())
 
     def gen3():
@@ -493,7 +494,7 @@ def test_illegal_sequences():
         yield(Msg('create'))
         yield(Msg('configure', motor, {}))
 
-    with assert_raises(IllegalMessageSequence):
+    with pytest.raises(IllegalMessageSequence):
         RE(gen3())
 
 
@@ -517,7 +518,7 @@ def test_new_ev_desc():
 
     descs.clear()
     RE(gen1(), {'descriptor': collect_descs})
-    assert_equal(len(descs), 2)
+    assert len(descs) == 2
 
     def gen2():
         # configure between two events and explicitly before any events
@@ -535,7 +536,7 @@ def test_new_ev_desc():
 
     descs.clear()
     RE(gen2(), {'descriptor': collect_descs})
-    assert_equal(len(descs), 2)
+    assert len(descs) == 2
 
     def gen3():
         # configure once before any events -> one desc
@@ -551,7 +552,7 @@ def test_new_ev_desc():
 
     descs.clear()
     RE(gen3(), {'descriptor': collect_descs})
-    assert_equal(len(descs), 1)
+    assert len(descs) == 1
 
 
 def test_bad_checkpoint():
@@ -559,4 +560,5 @@ def test_bad_checkpoint():
                 Msg('pause')]
     RE(bad_plan)
     # Resuming will cause us to write the same close_run twice.
-    assert_raises(IllegalMessageSequence, RE.resume)
+    with pytest.raises(IllegalMessageSequence):
+        RE.resume()
