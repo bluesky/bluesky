d5e7dc067af1babc715298410b97e6db7e61da62
Eric Dill <edill@bnl.gov>
2016-01-27 08:24:58 -0500

    TST: Skip test_suspenders if pcaspy is not available

--- bluesky/tests/test_suspenders.py
+++ bluesky/tests/test_suspenders.py
@@ -3,6 +3,8 @@ import sys
 import asyncio
 import time as ttime
 import pytest
+from multiprocessing import Process
+import signal
 from bluesky import Msg
 from bluesky.tests.utils import setup_test_run_engine
 
@@ -10,6 +12,45 @@ from bluesky.tests.utils import setup_test_run_engine
 RE = setup_test_run_engine()
 loop = asyncio.get_event_loop()
 
+pcaspy_process = None
+
+def setup_module():
+    try:
+        from pcaspy import Driver, SimpleServer
+    except ImportError as ie:
+        pytest.skip("pcaspy is not available. Skipping all suspenders test."
+                    "ImportError: {}".format(ie))
+        def to_subproc():
+
+            prefix = 'BSTEST:'
+            pvdb = {
+                'VAL': {
+                    'prec': 3,
+                },
+            }
+
+            class myDriver(Driver):
+                def __init__(self):
+                    super(myDriver, self).__init__()
+
+            server = SimpleServer()
+            server.createPV(prefix, pvdb)
+            driver = myDriver()
+
+            # process CA transactions
+            while True:
+                try:
+                    server.process(0.1)
+                except KeyboardInterrupt:
+                    break
+
+        pcaspy_process = Process(target=to_subproc)
+        pcaspy_process.start()
+
+def teardown_module():
+    if pcaspy_process is not None:
+        os.kill(ppcaspy_processpid, signal.SIGINT)
+        pcaspy_process.join()
 
 def test_epics_smoke():
 
--- run_tests.py
+++ run_tests.py
@@ -1,59 +1,6 @@
 #!/usr/bin/env python
-
-# tests require pytest-cov and pytest-xdist
-import os
-import signal
 import sys
 import pytest
 
-try:
-    from pcaspy import Driver, SimpleServer
-    from multiprocessing import Process
-
-    def to_subproc():
-
-        prefix = 'BSTEST:'
-        pvdb = {
-            'VAL': {
-                'prec': 3,
-            },
-        }
-
-        class myDriver(Driver):
-            def __init__(self):
-                super(myDriver, self).__init__()
-
-        if __name__ == '__main__':
-            server = SimpleServer()
-            server.createPV(prefix, pvdb)
-            driver = myDriver()
-
-            # process CA transactions
-            while True:
-                try:
-                    server.process(0.1)
-                except KeyboardInterrupt:
-                    break
-
-    p = Process(target=to_subproc)
-except ImportError:
-    p = None
-
-
-def run():
-    if p is not None:
-        p.start()
-    try:
-        # adding rxs to show extra info on skips and xfails
-        ret = pytest.main()
-    finally:
-        if p is not None:
-            os.kill(p.pid, signal.SIGINT)
-            p.join()
-
-    return ret
-
 if __name__ == '__main__':
-    return_code = run()
-
-    sys.exit(return_code)
+    sys.exit(pytest.main())
