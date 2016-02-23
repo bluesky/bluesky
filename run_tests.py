8eb0ea6f623d6fdda8bc1eed63d50d0f3fd0fa0c
Eric Dill <edill@bnl.gov>
2016-02-23 10:02:51 -0500

    tst: Don't use pytest-cov.
    Run with coverage run run_tests.py to capture all info

--- run_tests.py
+++ run_tests.py
@@ -6,9 +6,9 @@ if __name__ == '__main__':
     # show output results from every test function
     args = ['-v']
     # show the message output for skipped and expected failure tests
-    args.append('-rxs')
-    # compute coverage stats for bluesky
-    args.extend(['--cov', 'bluesky'])
+    args.append('-vrxs')
+    # # compute coverage stats for bluesky
+    # args.extend(['--cov', 'bluesky'])
     # call pytest and exit with the return code from pytest so that
     # travis will fail correctly if tests fail
     sys.exit(pytest.main(args))
