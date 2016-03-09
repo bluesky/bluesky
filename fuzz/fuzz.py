c8c107e5108a4523faefcdf8229b0a4998dac3c8
Eric Dill <edill@bnl.gov>
2016-03-09 12:00:32 -0500

    Major overhaul of fuzzing

--- fuzz/fuzz.py
+++ fuzz/fuzz.py
@@ -1,10 +1,99 @@
+import os
+import random
+import signal
+import time as ttime
+import pdb
+from pprint import pprint
+from bluesky import IllegalMessageSequence
 from bluesky.examples import *
 from bluesky.tests.utils import setup_test_run_engine
-import random
-from bluesky import IllegalMessageSequence
+import uuid
+import itertools
+random.seed(2016)
 
-motors = [motor, motor1, motor2, motor3]
-dets = [det, det1, det2, det3, noisy_det]
+def unique_name():
+    return str(uuid.uuid4())[:7]
+
+
+# create objects
+all_objects = set()
+# create functions to get objects
+def get_magic_flyer():
+    """Get a new magic flyer. Magic flyers require no additional arguments in
+    their message: Msg('kickoff', get_magic_flyer())
+    """
+    det1 = get_1d_det()
+    motor = det1._motor
+    det2 = get_1d_det()
+    det2._motor = motor
+    det2._motor_field = motor.name
+    scan_points = random.choice(range(15))
+    flyname = 'flymagic_' + unique_name()
+    flymagic = FlyMagic(flyname, motor, det1, det2, scan_points=scan_points)
+    all_objects.add(flymagic)
+    return
+
+
+def get_flyer():
+    """Create a new flyer object. Flyers require start, stop, steps in their
+    message: Msg('kickoff', get_flyer(), start, stop, step)
+
+    Returns
+    -------
+    MockFlyer object
+    """
+    det = get_1d_det()
+    flyer = MockFlyer(det, det._motor)
+    all_objects.add(flyer)
+
+
+def get_1d_det():
+    """Create a new 1d detector
+
+    Returns
+    -------
+    SynGauss object
+    """
+    motor = get_motor()
+    detname = 'det1d_' + unique_name()
+    det = SynGauss(detname, motor, motor.name, center=random.randrange(-1, 1),
+                   Imax=random.randrange(1, 10), sigma=random.randrange(0, 5))
+    all_objects.add(det)
+    return det
+
+
+def get_2d_det():
+    """Create a new 2d detector
+
+    Returns
+    -------
+    Syn2DGauss object
+    """
+    x = get_motor()
+    y = get_motor()
+    detname = 'det2d_' + unique_name()
+    det = Syn2DGauss(detname, x, x.name, y, x.name,
+                     center=(random.sample(range(-10, 10), 2)),
+                     Imax=random.randrange(1, 10),
+                     sigma=random.randrange(0, 5),
+                     noise=random.choice(['poission', 'uniform', None]),
+                     noise_multiplier=random.random(0, 5))
+    all_objects.add(det)
+    return det
+
+
+def get_motor():
+    """Create a new motor object
+
+    Returns
+    -------
+    Mover object
+    """
+    mtr_name = 'mtr_' + unique_name()
+    mtr = Mover(mtr_name, [mtr_name], sleep_time=random.random())
+    all_objects.add(mtr)
+    return mtr
+
 
 # get a list of messages
 def get_messages(source_file):
@@ -29,6 +118,41 @@ def get_messages(source_file):
     return message_objects
 
 
+def interrupt_func():
+    pid = os.getpid()
+    os.kill(pid, signal.SIGINT)
+    ttime.sleep(0.5)
+
+
+def kill_func():
+    pid = os.getpid()
+    os.kill(pid, signal.SIGINT)
+    ttime.sleep(0.05)
+    os.kill(pid, signal.SIGINT)
+
+
+def spam_sigint():
+    pid = os.getpid()
+    for _ in range(100):
+        os.kill(pid, signal.SIGINT)
+        ttime.sleep(0.005)
+
+# define the random message generation functions
+def kickoff_and_collect(block=False, magic=False):
+    args = []
+    kwargs = {}
+    if magic:
+        flyer = get_magic_flyer()
+    else:
+        flyer = get_flyer()
+        args = [random.randrange(-5, -1),  # start
+                random.randrange(0, 5),    # stop
+                random.randint(1, 10)]     # step
+
+    if block:
+        kwargs = {'block_group': unique_name()}
+    return [Msg('kickoff', flyer, *args, **kwargs), Msg('collect', flyer)]
+
 
 def run_fuzz():
     from bluesky import examples
@@ -36,11 +160,23 @@ def run_fuzz():
     source_file = examples.__file__
     print("Using the following message objects")
 
-    message_objects = get_messages(source_file)
+    # create 10 different flyers with corresponding kickoff and collect
+    # messages
+    flyer_messages = [msg for _ in range(10)
+                      for msg in kickoff_and_collect(block=random.random()>0.5,
+                                                     magic=random.random()>0.5)]
+    set_messages = [Msg('set', motor, i) for i, motor in
+                    itertools.product(range(-5, 6),
+                                      [get_motor() for _ in range(5)])]
+    read_messages = [Msg('read', obj) for obj in all_objects]
+    trigger_messages = [Msg('trigger', obj) for obj in all_objects
+                        if hasattr(obj, 'trigger')]
+    stage_messages = [Msg('stage', obj) for obj in all_objects]
+    unstage_messages = [Msg('unstage', obj) for obj in all_objects]
     # add some messages to set a motor to random positions
-    message_objects.extend([Msg('set', motor, i) for i in range(-5, 6)])
-    # add some configure messages
-    message_objects.extend([Msg('configure', obj, {}) for obj in motors+dets])
+    message_objects = (flyer_messages + set_messages + read_messages +
+                       trigger_messages + stage_messages + unstage_messages)
+    print("Using the following messages")
     for obj in message_objects:
         print(obj)
 
@@ -48,22 +184,46 @@ def run_fuzz():
     print(set(RE._command_registry.keys()) -
           set([msg.command for msg in message_objects]))
 
+    num_message = 100
+    num_interrupt = 2
+    num_kill = 2
+    num_hammer_ctrl_c = 2
+    message = ('message', lambda: random.choice(message_objects), ['idle'])
+    interrupt = ('interrupt', interrupt_func, ['paused', 'idle'])
+    kill = ('kill', kill_func, ['idle'])
+    hammer_ctrl_c = ('spam ctrl+c', spam_sigint, ['idle'])
+
+    actions = ([message] * num_message +
+               [interrupt] * num_interrupt +
+               [kill] * num_kill +
+               [hammer_ctrl_c] * num_hammer_ctrl_c)
+
     msg_seq = []
     count = 0
-    while count < 100:
-        msg = random.choice(message_objects)
-        msg_seq.append(msg)
+    while count < 250:
+        name, action, expected_state = random.choice(actions)
+        if name == 'message':
+            try:
+                msg = action()
+                msg_seq.append(msg)
+                RE([msg])
+            except IllegalMessageSequence as err:
+                print(err)
+            # except Exception as e:
+            #     pdb.set_trace()
+        else:
+            msg_seq.append(name)
         try:
-            RE([msg])
-        except IllegalMessageSequence as err:
-            print(err)
-        assert RE.state == 'idle'
+            assert RE.state in expected_state
+        except AssertionError:
+            pdb.set_trace()
         count += 1
         if count % 100 == 0:
             print('processed %s messages' % count)
 
     print("Fuzz testing completed successfully")
-
+    print("Actions taken in the following order")
+    pprint(msg_seq)
 
 
 if __name__ == "__main__":
