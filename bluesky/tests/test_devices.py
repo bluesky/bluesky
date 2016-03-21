dccf25865222280a29718ff174399787b57aeee7
Eric Dill <edill@bnl.gov>
2016-03-21 15:02:32 -0400

    TST: Skip the devices test if ophyd is not available

--- bluesky/tests/test_devices.py
+++ bluesky/tests/test_devices.py
@@ -1,30 +1,43 @@
-from ophyd import Component as Cpt, Device, Signal as UnusableSignal
 from bluesky.utils import ancestry, share_ancestor, separate_devices
 from bluesky.tests.utils import setup_test_run_engine
 from bluesky import Msg
+import pytest
+
+# some module level globals.
+ophyd = None
+reason = ''
+
+try:
+    import ophyd
+    from ophyd import Component as Cpt, Device, Signal as UnusableSignal
+except ImportError as ie:
+    # pytestmark = pytest.mark.skip
+    ophyd = None
+    reason = str(ie)
+else:
+    # define the classes only if ophyd is available
+    class Signal(UnusableSignal):
+        def __init__(self, _, *, name=None, parent=None):
+            super().__init__(name=name, parent=parent)
+    class A(Device):
+        s1 = Cpt(Signal, '')
+        s2 = Cpt(Signal, '')
+    class B(Device):
+        a1 = Cpt(A, '')
+        a2 = Cpt(A, '')
+
+# define a skip condition based on if ophyd is available or not
+requires_ophyd = pytest.mark.skipif(ophyd is None, reason=reason)
 
 
-class Signal(UnusableSignal):
-    def __init__(self, _, *, name=None, parent=None):
-        super().__init__(name=name, parent=parent)
-
-
-class A(Device):
-    s1 = Cpt(Signal, '')
-    s2 = Cpt(Signal, '')
-
-
-class B(Device):
-    a1 = Cpt(A, '')
-    a2 = Cpt(A, '')
-
-
+@requires_ophyd
 def test_ancestry():
     b = B('')
     assert ancestry(b) == [b]
     assert ancestry(b.a1) == [b.a1, b]
     assert ancestry(b.a1.s1) == [b.a1.s1, b.a1, b]
 
+@requires_ophyd
 def test_share_ancestor():
     b1 = B('')
     b2 = B('')
@@ -36,6 +49,7 @@ def test_share_ancestor():
     assert not share_ancestor(b2, b1.a1.s1)
 
 
+@requires_ophyd
 def test_separate_devices():
     b1 = B('')
     b2 = B('')
@@ -47,6 +61,7 @@ def test_separate_devices():
     assert separate_devices([b1, a]) == [b1, a]
 
 
+@requires_ophyd
 def test_monitor():
     docs = []
     def collect(name, doc):
