6e83e0a08813f31c532bede02f0d9f53df5eb14d
danielballan <daniel.b.allan@gmail.com>
2016-04-06 13:24:04 -0500

    CLN: Remove hardware_checklist, to be packaged separately.

--- bluesky/standard_config.py
+++ bluesky/standard_config.py
@@ -8,12 +8,6 @@ import logging
 import warnings
 from bluesky.run_engine import RunEngine
 from bluesky.register_mds import register_mds
-from bluesky.hardware_checklist import (connect_olog,
-                                        connect_channelarchiver,
-                                        check_storage, connect_pv,
-                                        assert_pv_equal, assert_pv_greater,
-                                        assert_pv_less, assert_pv_in_band,
-                                        assert_pv_out_of_band)
 from bluesky.global_state import gs, abort, stop, resume, panic, all_is_well
 from bluesky.spec_api import *
 from bluesky.callbacks import LiveTable, LivePlot, LiveMesh, print_metadata
@@ -105,89 +99,3 @@ def olog_wrapper(logbook, logbooks):
 def show_debug_logs():
     logging.basicConfig()
     logging.getLogger('bluesky').setLevel(logging.DEBUG)
-
-
-def basic_checklist(ca_url=None, disk_storage=None, pv_names=None,
-                    pv_conditions=None, swallow_errors=False):
-    """
-    Run checklist of functions that ensure the setup is working.
-
-    Not all of these items are required for bluesky to run and collect data.
-    Nonetheless, if you ignore failures you ignore them at your own risk!
-
-    - Check that storage disks have free space available.
-    - Check connection to metadatastore and filestore databases.
-    - Check connection to the olog.
-    - Check connection to the channel archiver.
-    - Check that certain PVs are responsive.
-    - Check that readings from certain PVs have a reasonable value.
-
-    Parameters
-    ----------
-    ca_url : string, optional
-        url to channel archiver
-    disk_storage : list of tuples, optional
-        List of pairs, giving a path to a disk and bytes of free space needed.
-        For example, [('path/to/disk', minimum_required_bytes_free),]
-    pv_names : list, optional
-        List of PVs to run 'caget' on and require a timely response.
-        For example, ['PV:CAT', 'PV:DOG']
-    pv_conditions : list of tuples, optional
-        List of conditions about the values of certain PVs.
-        For example, [(pv_name, message, func, *args),]
-        The func should be one of the following functions in
-        bluesky.hardware_checklist: assert_equal, assert_greater,
-        assert_less, assert_in_band, assert_out_of_band.
-
-    Examples
-    --------
-    >>> checklist('http://xf23id-ca.cs.nsls2.local:4800',
-                  [('/home', 1000000), ('/GPFS', 10000000)],
-                  ['PV:RED', 'PV:BLUE'],
-                  [('PV:CAT', 'cat is 5', assert_pv_equal, 5),
-                   ('PV:DOG', 'dog is at least 4', assert_pv_greater, 4),
-                   ('PV:BEAR', 'bear is 4-6', assert_pv_in_band, 4, 6)])
-    """
-    print("  Attempting to connect to the olog...")
-    _try_and_print(connect_olog, swallow_errors=swallow_errors)
-
-    if ca_url is None:
-        print("- Skipping channel arciver check; no URL was provided.")
-    else:
-        print("  Attempting to connect to the channel archiver...")
-        _try_and_print(connect_channelarchiver, ca_url,
-                       swallow_errors=swallow_errors)
-    if disk_storage is None:
-        print("- Skipping storage disk check; no disks were specified.")
-    else:
-        for disk, required_free in disk_storage:
-            print("  Checking that %s has at least %d bytes free..." %
-                  (disk, required_free))
-            _try_and_print(check_storage, disk, required_free,
-                           swallow_errors=swallow_errors)
-
-    if pv_names is None:
-        print("- Skipping PV responsiveness checks; no PV names were given.")
-    else:
-        for pv_name in pv_names:
-            print("  Checking that the PV '%s' is responsive..." % pv_name)
-            _try_and_print(connect_pv, pv_name, swallow_errors=swallow_errors)
-
-    if pv_conditions is None:
-        print("- Skipping PV conditions; none were specified.")
-    else:
-        for pv_name, msg, func, *args in pv_conditions:
-            print("  Checking that %s..." % msg)
-            _try_and_print(func, pv_name, *args, swallow_errors=swallow_errors)
-
-
-def _try_and_print(func, *args, swallow_errors=False):
-    "Gratuitous function to print a checkmark or X on the preceding line"
-    try:
-        func(*args)
-    except:
-        print('\x1b[1A\u2717')
-        if not swallow_errors:
-            raise
-    else:
-        print('\x1b[1A\u2713')
