d4a997d7e154f69447703d4c5f065581a7a70563
Eric Dill <edill@bnl.gov>
2016-04-14 15:01:42 -0400

    MNT: Fix warning import location

--- bluesky/qt_kicker.py
+++ bluesky/qt_kicker.py
@@ -4,5 +4,5 @@ from .utils import install_qt_kicker as _install_qt_kicker
 
 def install_qt_kicker():
     warnings.warn("The qt_kicker module is deprecated. Instead, use "
-                  "`from bluesky.utils import qt_kicker`")
+                  "`from bluesky.utils import install_qt_kicker`")
     _install_qt_kicker()
