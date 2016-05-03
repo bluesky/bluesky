75daae328a32a0d2e77339fca90c8dd1a4ece468
Thomas A Caswell <tcaswell@bnl.gov>
2016-05-03 01:16:06 -0400

    TST: test suspenders using soft signals

---
+++ bluesky/tests/test_suspenders.py
@@ -0,0 +1,54 @@
+import pytest
+import ophyd
+import sys
+from bluesky.suspenders import (PVSuspendBoolHigh,
+                                PVSuspendBoolLow,
+                                PVSuspendFloor,
+                                PVSuspendCeil,
+                                PVSuspendInBand,
+                                PVSuspendOutBand)
+from bluesky import RunEngine, Msg
+import time as ttime
+
+
+@pytest.mark.parametrize(
+    'klass,sc_args,start_val,fail_val,resume_val,wait_time',
+    [(PVSuspendBoolHigh, (), 0, 1, 0, .5),
+     (PVSuspendBoolLow, (), 1, 0, 1, .5),
+     (PVSuspendFloor, (.5,), 1, 0, 1, .5),
+     (PVSuspendCeil, (.5,), 0, 1, 0, .5),
+     (PVSuspendInBand, (.5, 1.5), 1, 0, 1, .5),
+     (PVSuspendOutBand, (.5, 1.5), 0, 1, 0, .5)])
+def test_suspender(klass, sc_args, start_val, fail_val,
+                   resume_val, wait_time):
+    RE = RunEngine({})
+    loop = RE._loop
+    if sys.platform == 'darwin':
+        pytest.xfail('OSX event loop is different; resolve this later')
+    sig = ophyd.Signal()
+    my_suspender = klass(sig,
+                         *sc_args, sleep=wait_time)
+    my_suspender.install(RE)
+
+    def putter(val):
+        sig.put(val)
+
+    # make sure we start at good value!
+    putter(start_val)
+    # dumb scan
+    scan = [Msg('checkpoint'), Msg('sleep', None, .2)]
+    RE(scan)
+    # paranoid
+    assert RE.state == 'idle'
+
+    start = ttime.time()
+    # queue up fail and resume conditions
+    loop.call_later(.1, putter, fail_val)
+    loop.call_later(1, putter, resume_val)
+    # start the scan
+    RE(scan)
+    stop = ttime.time()
+    # assert we waited at least 2 seconds + the settle time
+    delta = stop - start
+    print(delta)
+    assert delta > 1 + wait_time + .2
