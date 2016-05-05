e9afb02fbf41d996c735d611efd7d4bdb7287167
danielballan <daniel.b.allan@gmail.com>
2016-05-05 17:47:27 -0400

    TST: Test contexts.

--- bluesky/tests/test_new_examples.py
+++ bluesky/tests/test_new_examples.py
@@ -1,11 +1,22 @@
+from collections import deque
 import pytest
 from bluesky import Msg
-from bluesky.examples import det
+from bluesky.examples import det, det1, det2
 from bluesky.plans import (create, save, read, monitor, unmonitor, null,
                            abs_set, rel_set, trigger, sleep, wait, checkpoint,
                            clear_checkpoint, pause, deferred_pause, kickoff,
                            collect, configure, stage, unstage, subscribe,
-                           unsubscribe, open_run, close_run, wait_for)
+                           unsubscribe, open_run, close_run, wait_for,
+                           subs_context, run_context, event_context,
+                           baseline_context, monitor_context,
+                           stage_context, planify, finalize, fly_during,
+                           lazily_stage, relative_set, reset_positions,
+                           configure_count_time, trigger_and_read,
+                           repeater, repeater, caching_repeater)
+
+
+def cb(name, doc):
+    pass
 
 @pytest.mark.parametrize(
     'plan,plan_args,plan_kwargs,msgs',
@@ -54,3 +65,59 @@ from bluesky.plans import (create, save, read, monitor, unmonitor, null,
     ])
 def test_stub_plans(plan, plan_args, plan_kwargs, msgs):
     assert list(plan(*plan_args, **plan_kwargs)) == msgs
+
+
+@pytest.mark.parametrize(
+    'cm,args,kwargs,before,after',
+    [(baseline_context, ([det1, det2],), {},
+      [Msg('trigger', det1),
+       Msg('trigger', det2),
+       Msg('wait'),
+       Msg('create', None, name='baseline'),
+       Msg('read', det1),
+       Msg('read', det2),
+       Msg('save')],
+      [Msg('trigger', det1),
+       Msg('trigger', det2),
+       Msg('wait'),
+       Msg('create', None, name='baseline'),
+       Msg('read', det1),
+       Msg('read', det2),
+       Msg('save')]),
+     (stage_context, ([det1, det2],), {},
+      [Msg('stage', det1),
+       Msg('stage', det2)],
+      [Msg('unstage', det2),
+       Msg('unstage', det1)]),
+     (subs_context, ({'all': [cb]},), {},
+      [Msg('subscribe', None, 'all', cb)],
+      [Msg('unsubscribe', None, token=None)]),
+     (monitor_context, (['sig'],), {},
+      list(monitor('sig')),
+      list(unmonitor('sig'))),
+     (event_context, (), {},
+      [Msg('create', name='primary')],
+      [Msg('save')]),
+     (run_context, (), {'md': {'a': 1}},
+      [Msg('open_run', a=1)],
+      [Msg('close_run')]),
+    ])
+def test_plan_contexts(cm, args, kwargs, before, after):
+    @planify
+    def plan():
+        ps = deque()
+        with cm(ps, *args, **kwargs):
+            ps.append(['sentinel'])
+        return ps
+
+    actual_before = []
+    actual_after = []
+    target = actual_before
+    for msg in plan():
+        if msg == 'sentinel':
+            target = actual_after
+            continue
+        msg.kwargs.pop('group', None)
+        target.append(msg)
+    assert actual_before == before
+    assert actual_after == after
