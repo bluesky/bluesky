a14af21889e5b8a90a27e4627337323afc6efe4a
Thomas A Caswell <tcaswell@bnl.gov>
2016-08-22 10:42:53 -0400

    DOC: update example

--- examples/basic_raster_plot.py
+++ examples/basic_raster_plot.py
@@ -2,14 +2,16 @@ import numpy as np
 import bluesky.callbacks
 import matplotlib.pyplot as plt
 from cycler import cycler
+import uuid
 
 xct = 10
 yct = 15
 ict = xct * yct
 
-cb = bluesky.callbacks.LiveRaster((yct, xct),  'I', clim=[0, 1])
+cb = bluesky.callbacks.LiveRaster((yct, xct),  'I', clim=[0, 1],
+                                  aspect='auto')
 
-cb('start', {})
+cb('start', {'scan_id': 1, 'uid': str(uuid.uuid4())})
 cy = ((cycler('x', np.linspace(0, 1, xct, endpoint=True)) *
        cycler('y', np.linspace(0, 1, yct, endpoint=True))) +
       cycler('I', np.linspace(0, 1, ict, endpoint=True)))
