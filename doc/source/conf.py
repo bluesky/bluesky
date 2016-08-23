ff574963da45632bcca34ffdb03e941983377809
danielballan <daniel.b.allan@gmail.com>
2016-08-22 22:31:37 -0400

    DOC: Show the correct version in the docs.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -69,7 +69,8 @@ author = 'Brookhaven National Lab'
 # built documents.
 #
 # The short X.Y version.
-version = '0.0.1'
+import bluesky
+version = bluesky.__version__
 # The full version, including alpha/beta/rc tags.
 release = '0.0.1'
 
