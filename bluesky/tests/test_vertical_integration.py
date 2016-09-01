a42c9b3825c8545f0390449a581c03a53dd06c24
Thomas A Caswell <tcaswell@bnl.gov>
2016-09-01 15:04:40 -0400

    API: fully remove register_mds
    This was removed from the top-level in 0.5.0.
    
    Fix the tests this would break.

--- bluesky/tests/test_vertical_integration.py
+++ bluesky/tests/test_vertical_integration.py
@@ -1,54 +1,30 @@
 from collections import defaultdict
-from bluesky.register_mds import register_mds
-from bluesky.global_state import gs
 from bluesky.examples import stepscan, det, motor
-import pytest
+from bluesky.callbacks.broker import post_run, verify_files_saved
+from functools import partial
 
 
-def setup_module(module):
-    try:
-        import metadatastore
-    except ImportError as ie:
-        raise pytest.skip('ImportError: {0}'.format(ie))
-    else:
-        from metadatastore.test.utils import mds_setup
-        mds_setup()
-        register_mds(gs.RE)
-
-def teardown_module(module):
-    from metadatastore.test.utils import mds_teardown
-    mds_teardown()
-
-
-def test_scan_and_get_data():
-    try:
-        from databroker import DataBroker as db
-        from bluesky.global_state import gs
-    except ImportError as ie:
-        raise pytest.skip('skipping because some libary is unavailable\n'
-                          'ImportError:  is:{}'.format(ie))
-    uid, = gs.RE(stepscan(det, motor), group='foo', beamline_id='testing',
-             config={})
+def test_scan_and_get_data(fresh_RE, db):
+    RE = fresh_RE
+    RE.subscribe('all', db.mds.insert)
+    uid, = RE(stepscan(det, motor), group='foo', beamline_id='testing',
+              config={})
 
     hdr = db[uid]
     db.fetch_events(hdr)
 
 
-def test_post_run():
-    try:
-        import databroker
-        from bluesky.global_state import gs
-        from bluesky.callbacks.broker import post_run
-    except ImportError as ie:
-        raise pytest.skip('skipping because some libary is unavailable\n'
-                          'ImportError:  is:{}'.format(ie))
+def test_post_run(fresh_RE, db):
+    RE = fresh_RE
+    RE.subscribe('all', db.mds.insert)
     output = defaultdict(list)
+
     def do_nothing(doctype, doc):
         output[doctype].append(doc)
 
-    gs.RE.ignore_callback_exceptions = False
+    RE.ignore_callback_exceptions = False
 
-    gs.RE(stepscan(det, motor), subs={'stop': [post_run(do_nothing)]})
+    RE(stepscan(det, motor), subs={'stop': [post_run(do_nothing, db=db)]})
     assert len(output)
     assert len(output['start']) == 1
     assert len(output['stop']) == 1
@@ -56,12 +32,9 @@ def test_post_run():
     assert len(output['event']) == 10
 
 
-def test_verify_files_saved():
-    try:
-        import databroker
-        from bluesky.global_state import gs
-        from bluesky.callbacks.broker import verify_files_saved
-    except ImportError as ie:
-        raise pytest.skip('skipping because some libary is unavailable\n'
-                          'ImportError:  is:{}'.format(ie))
-    gs.RE(stepscan(det, motor), subs={'stop': verify_files_saved})
+def test_verify_files_saved(fresh_RE, db):
+    RE = fresh_RE
+    RE.subscribe('all', db.mds.insert)
+
+    vfs = partial(verify_files_saved, db=db)
+    RE(stepscan(det, motor), subs={'stop': vfs})
