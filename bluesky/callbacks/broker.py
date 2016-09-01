fbdebdb69da350faff3b6e8e741ba0529b517332
Thomas A Caswell <tcaswell@bnl.gov>
2016-09-01 15:03:13 -0400

    API/ENH: pass optional databroker into callbacks
    Add a `db` kwarg to pass in an explicit db object.  If not passed,
    get databroker singleton.

--- bluesky/callbacks/broker.py
+++ bluesky/callbacks/broker.py
@@ -1,10 +1,5 @@
 import os
 import time as ttime
-from databroker import DataBroker as db, get_events, process
-from databroker.databroker import fill_event
-import filestore.api as fsapi
-import matplotlib.pyplot as plt
-from xray_vision.backend.mpl.cross_section_2d import CrossSection
 from .core import CallbackBase
 import numpy as np
 import doct
@@ -20,6 +15,8 @@ class LiveImage(CallbackBase):
         name of data field in an Event
     """
     def __init__(self, field):
+        from xray_vision.backend.mpl.cross_section_2d import CrossSection
+        import matplotlib.pyplot as plt
         super().__init__()
         self.field = field
         fig = plt.figure()
@@ -27,15 +24,15 @@ class LiveImage(CallbackBase):
         self.cs._fig.show()
 
     def event(self, doc):
+        import filestore.api as fsapi
         uid = doc['data'][self.field]
         data = fsapi.retrieve(uid)
         self.cs.update_image(data)
         self.cs._fig.canvas.draw_idle()
 
 
-def post_run(callback):
-    """
-    Trigger a callback to process all the Documents from a run at the end.
+def post_run(callback, db=None):
+    """Trigger a callback to process all the Documents from a run at the end.
 
     This function does not receive the Document stream during collection.
     It retrieves the complete set of Documents from the DataBroker after
@@ -48,6 +45,10 @@ def post_run(callback):
 
             def func(doc_name, doc):
                 pass
+
+    db : Broker, optional
+        The databroker instance to use, if not provided use databroker
+        singleton
 
     Returns
     -------
@@ -68,16 +69,21 @@ def post_run(callback):
     |         2  |  14:02:32.158503  |          5.00  |          2.00  |
     |         1  |  14:02:32.099807  |          5.00  |          1.00  |
     +------------+-------------------+----------------+----------------+
+
     """
+
+    if db is None:
+        from databroker import DataBroker as db
+
     def f(name, stop_doc):
         if name != 'stop':
             return
         uid = stop_doc['run_start']
-        return process(db[uid], callback)
+        return db.process(db[uid], callback)
     return f
 
 
-def make_restreamer(callback):
+def make_restreamer(callback, db=None):
     """
     Run a callback whenever a uid is updated.
 
@@ -85,6 +91,11 @@ def make_restreamer(callback):
     ----------
     callback : callable
         expected signature is `f(name, doc)`
+
+    db : Broker, optional
+        The databroker instance to use, if not provided use databroker
+        singleton
+
 
     Example
     -------
@@ -99,13 +110,21 @@ def make_restreamer(callback):
 
     >>> last_uid_signal.subscribe(g)
     """
+    from databroker import process
+
+    if db is None:
+        from databroker import DataBroker as db
+
     def cb(value, **kwargs):
         return process(db[value], callback)
     return cb
 
 
-def verify_files_saved(name, doc):
+def verify_files_saved(name, doc, db=None):
     "This is a brute-force approach. We retrieve all the data."
+    if db is None:
+        from databroker import DataBroker as db
+
     ttime.sleep(0.1)  # Wati for data to be saved.
     if name != 'stop':
         return
@@ -119,7 +138,7 @@ def verify_files_saved(name, doc):
         print('\x1b[1A\u2713')  # print a checkmark on the previous line
     print("  Verifying that all externally-stored files are accessible...")
     try:
-        list(get_events(header, fill=True))
+        list(db.get_events(header, fill=True))
     except Exception as e:
         print("  Verification Failed! Error: {0}".format(e))
     else:
@@ -145,12 +164,16 @@ class LiveTiffExporter(CallbackBase):
         default to False; if True, do not write any files
     overwrite : bool
         default to False, raising an OSError if file exists
+    db : Broker, optional
+        The databroker instance to use, if not provided use databroker
+        singleton
 
     Attributes
     ----------
     filenames : list of filenames written in ongoing or most recent run
     """
-    def __init__(self, field, template, dryrun=False, overwrite=False):
+    def __init__(self, field, template, dryrun=False, overwrite=False,
+                 db=None):
         try:
             import tifffile
         except ImportError:
@@ -162,6 +185,12 @@ class LiveTiffExporter(CallbackBase):
         else:
             # stash a reference so the module is accessible in self._save_image
             self._tifffile = tifffile
+
+        if db is None:
+            from databroker import DataBroker as db
+
+        self.db = db
+
         self.field = field
         self.template = template
         self.dryrun = dryrun
@@ -190,7 +219,7 @@ class LiveTiffExporter(CallbackBase):
         doc = doct.Document('event', doc)
         if self.field not in doc['data']:
             return
-        fill_event(doc)  # modifies in place
+        self.db.fill_event(doc)  # modifies in place
         image = np.asarray(doc['data'][self.field])
         if image.ndim == 2:
             filename = self.template.format(start=self._start, event=doc)
