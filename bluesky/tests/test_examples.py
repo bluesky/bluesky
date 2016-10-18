2809db2f255a498e8d9310a3bc573ac1a9fc2f3c
christopher <cjwright4242@gmail.com>
2016-10-17 20:04:45 -0400

    FIX: add spec to handler

--- bluesky/tests/test_examples.py
+++ bluesky/tests/test_examples.py
@@ -685,7 +685,7 @@ def test_async_trigger_delay(motor_det, fresh_RE):
 
 def test_fs_reader(db):
     fs = db.fs
-    fs.register_handler(ReaderWithFSHandler)
+    fs.register_handler(ReaderWithFSHandler.specs, ReaderWithFSHandler)
     det = ReaderWithFileStore({'img': lambda: np.array(np.ones(10, 10))},
                               fs=fs)
     det.stage()
