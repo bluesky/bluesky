cfe1f5a414cc8981bb086c2370e350be575a470b
Thomas A Caswell <tcaswell@bnl.gov>
2016-12-16 20:08:16 -0500

    TST: add smoke test

---
+++ bluesky/tests/test_plan_tools.py
@@ -0,0 +1,8 @@
+from bluesky.plans import scan
+from bluesky.plan_tools import print_summary, print_summary_wrapper
+
+
+def test_print_summary(motor_det):
+    motor, det = motor_det
+    print_summary(scan([det], motor, -1, 1, 10))
+    list(print_summary_wrapper(scan([det], motor, -1, 1, 10)))
