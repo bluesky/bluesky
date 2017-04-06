7118065c4d99674b169121c7ec4ae6454c1df4ac
Thomas A Caswell <tcaswell@bnl.gov>
2017-04-06 10:23:42 -0400

    TST: do not skip ophyd tests if libca is not installed

--- bluesky/tests/__init__.py
+++ bluesky/tests/__init__.py
@@ -5,18 +5,10 @@ reason = ''
 
 try:
     import ophyd
-    from ophyd import setup_ophyd
 except ImportError as ie:
     # pytestmark = pytest.mark.skip
     ophyd = None
     reason = str(ie)
-else:
-    try:
-        setup_ophyd()
-        # define the classes only if ophyd is available
-    except Exception as E:
-        ophyd = None
-        reason = str(E)
 
 # define a skip condition based on if ophyd is available or not
 requires_ophyd = pytest.mark.skipif(ophyd is None, reason=reason)
