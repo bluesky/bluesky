b4aeb8e99f4d56def099ddb2a94c0c78e57db838
danielballan <daniel.b.allan@gmail.com>
2017-08-06 16:01:47 -0400

    API: Remove shim module deprecated in April 2016.

--- bluesky/qt_kicker.py
+++
@@ -1,8 +0,0 @@
-import warnings
-from .utils import install_qt_kicker as _install_qt_kicker
-
-
-def install_qt_kicker():
-    warnings.warn("The qt_kicker module is deprecated. Instead, use "
-                  "`from bluesky.utils import install_qt_kicker`")
-    _install_qt_kicker()
