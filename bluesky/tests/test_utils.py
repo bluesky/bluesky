f0851478e25b51881a96280597edc6bb1ca7766d
Thomas A Caswell <tcaswell@bnl.gov>
2017-10-25 16:10:29 -0400

    TST: expand cycler merging tests

--- bluesky/tests/test_utils.py
+++ bluesky/tests/test_utils.py
@@ -1,11 +1,14 @@
 import pytest
 from types import SimpleNamespace
+from functools import reduce
+import operator
 
 from bluesky.utils import ensure_generator, Msg, merge_cycler
 from cycler import cycler
 from ophyd.pseudopos import (PseudoPositioner, PseudoSingle,
                              real_position_argument, pseudo_position_argument)
 from ophyd.positioner import SoftPositioner
+from ophyd.signal import Signal
 from ophyd import (Component as C)
 
 
@@ -27,6 +30,8 @@ def hw():
         real1 = C(SoftPositioner, init_pos=0)
         real2 = C(SoftPositioner, init_pos=0)
         real3 = C(SoftPositioner, init_pos=0)
+
+        sig = C(Signal, value=0)
 
         @pseudo_position_argument
         def forward(self, pseudo_pos):
@@ -65,20 +70,63 @@ def hw():
             return self.PseudoPosition(pseudo1=-real_pos.real1)
 
     return SimpleNamespace(pseudo3x3=SPseudo3x3(name='pseudo3x3'),
-                           pseudo1x3=SPseudo1x3(name='pseudo1x3'))
+                           pseudo1x3=SPseudo1x3(name='pseudo1x3'),
+                           sig=Signal(name='sig', value=0))
 
 
-def test_cycler_merge_2pseudo(hw):
+@pytest.mark.parametrize('traj',
+                         ({'pseudo1': [10, 11, 12],
+                           'pseudo2': [20, 21, 22]},
+                          {'pseudo1': [10, 11, 12],
+                           'pseudo2': [20, 21, 22],
+                           'pseudo3': [30, 31, 32]},
+                          )
+                         )
+def test_cycler_merge_pseudo(hw, traj):
     p3x3 = hw.pseudo3x3
-    # put the real positions at 1, 2, 3
-    p3x3.set(-1, -2, -3)
-    r1_vec = [10, 11, 12]
-    r2_vec = [20, 21, 22]
-    cyc = cycler(p3x3.real1, r1_vec) + cycler(p3x3.real2, r2_vec)
+    sig = hw.sig
+    keys = traj.keys()
+    tlen = len(next(iter(traj.values())))
+    expected_merge = [{k: traj[k][j] for k in keys}
+                      for j in range(tlen)]
+
+    cyc = reduce(operator.add, (cycler(getattr(p3x3, k), v)
+                                for k, v in traj.items()))
+
+    mcyc = merge_cycler(cyc + cycler(sig, range(tlen)))
+
+    assert mcyc.keys == {p3x3, sig}
+    assert mcyc.by_key()[p3x3] == expected_merge
+    assert mcyc.by_key()[sig] == list(range(tlen))
+
+
+@pytest.mark.parametrize('children',
+                         (
+                             ['pseudo1', 'real1'],
+                             ['pseudo1', 'pseudo2', 'real1']))
+def test_cycler_merge_pseudo_real_clash(hw, children):
+    p3x3 = hw.pseudo3x3
+    cyc = reduce(operator.add, (cycler(getattr(p3x3, k), range(5))
+                                for k in children))
+
+    with pytest.raises(ValueError):
+        merge_cycler(cyc)
+
+
+@pytest.mark.parametrize('children',
+                         (
+                             ['pseudo1'],
+                             ['pseudo2', 'sig'],
+                             ['real1'],
+                             ['real1', 'real2'],
+                             ['real1', 'real2', 'sig'],
+                         ))
+def test_cycler_merge_mixed(hw, children):
+    p3x3 = hw.pseudo3x3
+    cyc = reduce(operator.add, (cycler(getattr(p3x3, k), range(5))
+                                for k in children))
 
     mcyc = merge_cycler(cyc)
-    expected_merge = [{'real1': r1, 'real2': r2}
-                      for r1, r2 in zip(r1_vec, r2_vec)]
-    assert len(mcyc.keys) == 1
-    assert mcyc.keys == {p3x3}
-    assert mcyc.by_key()[p3x3] == expected_merge
+
+    assert mcyc.keys == cyc.keys
+    assert mcyc.by_key() == cyc.by_key()
