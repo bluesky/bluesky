5f059639cf0c2b311e0991b9ef7b09eeb409821c
Thomas A Caswell <tcaswell@bnl.gov>
2017-10-25 21:27:59 -0400

    TST: add outer-product test of PseudoPositioner

--- bluesky/tests/test_plans.py
+++ bluesky/tests/test_plans.py
@@ -3,6 +3,7 @@ import pytest
 from bluesky.tests.utils import DocCollector
 import bluesky.plans as bp
 from bluesky.examples import motor, motor1, motor2, det
+import pandas as pd
 
 
 def _validate_start(start, expected_values):
@@ -64,6 +65,7 @@ def test_plan_header(fresh_RE, plan, target):
         _validate_start(s, target)
 
 
+
 def test_ops_dimension_hints(fresh_RE):
     RE = fresh_RE
     c = DocCollector()
@@ -78,3 +80,26 @@ def test_ops_dimension_hints(fresh_RE):
 
     assert st['hints']['dimensions'] == [
         (m.hints['fields'], 'primary') for m in (motor, motor1)]
+
+
+def test_mesh_pseudo(hw, fresh_RE):
+    p3x3 = hw.pseudo3x3
+    sig = hw.sig
+    RE = fresh_RE
+    d = DocCollector()
+
+    RE.subscribe(d.insert)
+    rs, = RE(bp.outer_product_scan([sig],
+                                   p3x3.pseudo1, 0, 3, 5,
+                                   p3x3.pseudo2, 7, 10, 7, False))
+    df = pd.DataFrame([_['data']
+                       for _ in d.event[d.descriptor[rs][0]['uid']]])
+
+    for k in p3x3.describe():
+        assert k in df
+
+    for k in sig.describe():
+        assert k in df
+
+    assert all(df[sig.name] == 0)
+    assert all(df[p3x3.pseudo3.name] == 0)
