BUG: fwhm calculation, and add tests on simple gaussian

bluesky/callbacks/fitting.py
@@ -281,8 +281,8 @@ class PeakStats(CollectThenCompute):
         if _cen_list:
             self.cen = np.mean(_cen_list)
 
-        if len(_cen_list) == 2:
-            self.fwhm = float(_cen_list[1] - _cen_list[0])
+        if len(_cen_list) >= 2:
+            self.fwhm = float(_cen_list[-1] - _cen_list[0])
 
         # reset y data
         y = self.y_data