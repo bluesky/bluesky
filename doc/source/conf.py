1d12af249abd847c213dcf8919f8f7e4814c1d80
Thomas A Caswell <tcaswell@bnl.gov>
2017-11-01 01:21:58 -0400

    DOC: use python3 links

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -309,7 +309,7 @@ texinfo_documents = [
 #texinfo_no_detailmenu = False
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/', None),
+    'python': ('https://docs.python.org/3/', None),
     'numpy': ('https://docs.scipy.org/doc/numpy/', None),
     'scipy': ('https://docs.scipy.org/doc/scipy/reference/', None),
     'pandas': ('https://pandas.pydata.org/pandas-docs/stable', None),
