53637659b9e7b4ea07fc7465d772e8b659795a3f
Thomas A Caswell <tcaswell@bnl.gov>
2018-04-04 18:40:23 -0400

    TST: fix remaining two tests that were missing 'resume' msg

--- bluesky/tests/test_run_engine.py
+++ bluesky/tests/test_run_engine.py
@@ -475,14 +475,14 @@ def _make_unrewindable_suspender_marker():
                  motor,
                  UnReplayableSynGauss('det', motor, 'motor', center=0, Imax=1),
                  ['set', 'trigger', 'sleep',
-                  'rewindable', 'wait_for', 'rewindable',
+                  'rewindable', 'wait_for', 'resume', 'rewindable',
                   'set', 'trigger']))
 
     inps.append((test_plan,
                  motor,
                  SynGauss('det', motor, 'motor', center=0, Imax=1),
                  ['set', 'trigger', 'sleep',
-                  'rewindable', 'wait_for', 'rewindable',
+                  'rewindable', 'wait_for', 'resume', 'rewindable',
                   'set',
                   'trigger', 'sleep', 'set', 'trigger']))
 
