2b0faaefa0ff92d79ca2f5e4ad2b7289d18ae36e
Pavol Juhas <pavol.juhas@gmail.com>
2018-11-01 17:36:52 -0400

    DEV: tell pyflakes unused names belong here

--- bluesky/callbacks/__init__.py
+++ bluesky/callbacks/__init__.py
@@ -1,3 +1,9 @@
+# declare module content to calm pyflakes about unused names
+__all__ = ["CallbackBase", "CallbackCounter", "print_metadata", "collector",
+           "get_obj_fields", "CollectThenCompute", "LiveTable", "LiveFit",
+           "LiveScatter", "LivePlot", "LiveGrid",
+           "LiveFitPlot", "LiveRaster", "LiveMesh"]
+
 from .core import (CallbackBase, CallbackCounter, print_metadata, collector,
                    get_obj_fields, CollectThenCompute, LiveTable)
 from .fitting import LiveFit
