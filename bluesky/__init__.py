ee5e91e44fecd764087bd0c9a711b5a65a476aea
Dan Allan <dallan@bnl.gov>
2018-11-15 16:19:09 -0500

    ENH: Use more fleixlbe set_handler instead of color_logs.

--- bluesky/__init__.py
+++ bluesky/__init__.py
@@ -5,7 +5,7 @@ from .utils import FailedStatus
 
 from .run_engine import RunEngine
 from .preprocessors import SupplementalData
-from .log import color_logs, plain_log_handler, color_log_handler
+from .log import set_handler
 
 from ._version import get_versions
 __version__ = get_versions()['version']
