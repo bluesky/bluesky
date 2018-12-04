8cce7fa5d73056950c90f9d0fc8e70670447b89b
christopher <cjwright4242@gmail.com>
2018-12-04 16:36:58 -0500

    better test of RunRouter, more coverage

--- bluesky/tests/test_callbacks.py
+++ bluesky/tests/test_callbacks.py
@@ -482,13 +482,23 @@ def test_run_router(RE, hw):
     LL = []
 
     def appender(start_doc):
-        L.append(('start', start_doc))
+        L.append(("start", start_doc))
         return lambda n, d: L.append((n, d))
 
-    rr = RunRouter([appender])
-    RE.subscribe(rr)
-    RE.subscribe(lambda n, d: LL.append((n, d)))
+    def not_interested(start_doc):
+        return
 
-    RE(bp.count([hw.det1], 1))
+    # Run with not interesteds
+    rr = RunRouter([not_interested])
+    rr_token = RE.subscribe(rr)
+
+    RE(bp.count([hw.img], 1))
+
+    # now we're interested
+    RE.unsubscribe(rr_token)
+    rr = RunRouter([appender, not_interested])
+    rr_token = RE.subscribe(rr)
+    RE.subscribe(lambda n, d: LL.append((n, d)))
+    RE(bp.count([hw.img], 1))
 
     assert L == LL
