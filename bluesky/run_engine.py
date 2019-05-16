7beb88ed85d7f577da44c218123369150544aadc
Garrett <bisc8233@gmail.com>
2019-05-16 11:07:19 -0400

    Update bluesky/run_engine.py
    Co-Authored-By: Maksim Rakitin <mrakitin@users.noreply.github.com>

--- bluesky/run_engine.py
+++ bluesky/run_engine.py
@@ -234,7 +234,7 @@ class RunEngine:
             import ophyd
             self.md['versions']['ophyd'] = ophyd.__version__
         except ImportError:
-            self.log.debug("Failed to import ophyd.__version__.")
+            self.log.debug("Failed to import ophyd.")
 
         from ._version import get_versions
         self.md['versions']['bluesky'] = get_versions()['version']
