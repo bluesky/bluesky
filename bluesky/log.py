45650c5c5752c1b59a159613d91415474c4263a4
ke-zhang-rd <kz2249@columbia.edu>
2019-05-29 19:08:03 -0400

    MNT: Clean

--- bluesky/log.py
+++ bluesky/log.py
@@ -104,10 +104,6 @@ class LogFormatter(logging.Formatter):
         message = []
         if hasattr(record, 'msg_command'):
             message.append('msg')
-        #if hasattr(record, 'doc_name'):
-        #    message.append('[%s]' % record.doc_name)
-        #if hasattr(record, 'doc_uid'):
-        #    message.append('%s' % record.doc_uid)
         message.append(record.getMessage())
         record.message = ' '.join(message)
         record.asctime = self.formatTime(record, self.datefmt)
