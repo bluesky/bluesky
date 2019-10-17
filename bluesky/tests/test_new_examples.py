d39c2d90ce74fdf322e5551042905904c2b42f1a
jklynch <jlynch@bnl.gov>
2019-10-17 08:32:46 -0400

    specify an initial value
    Co-Authored-By: Thomas A Caswell <tcaswell@gmail.com>

--- bluesky/tests/test_new_examples.py
+++ bluesky/tests/test_new_examples.py
@@ -642,7 +642,7 @@ def test_no_rewind_device(hw):
 
 def test_monitor(RE, hw):
     from ophyd.sim import SynSignal
-    signal = SynSignal(name='signal')
+    signal = SynSignal(name='signal', value=0.0)
     RE(monitor_during_wrapper(count([hw.det], 5), [signal]))
 
 
