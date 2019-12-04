d459483bcc3f91f9e36f47652bddbcee67de04fb
Pete Jemian <prjemian@gmail.com>
2019-12-04 16:05:05 -0600

    fixes #1270

--- bluesky/utils.py
+++ bluesky/utils.py
@@ -1486,3 +1486,54 @@ def _rearrange_into_parallel_dicts(readings):
         data[key] = payload['value']
         timestamps[key] = payload['timestamp']
     return data, timestamps
+
+
+def plot_prune_fifo(bec, n, y, x):
+    """
+    find the plot with axes x and y and replot with only the last *n* lines
+
+    Note: this is not a bluesky plan.  Call it as normal Python function.
+
+    EXAMPLE::
+
+        plot_prune_fifo(bec, 1, noisy, m1)
+
+    PARAMETERS
+    
+    bec : object
+        instance of BestEffortCallback
+    
+    n : int
+        number of plots to keep
+    
+    y : object
+        instance of ophyd.Signal (or subclass), 
+        dependent (y) axis
+    
+    x : object
+        instance of ophyd.Signal (or subclass), 
+        independent (x) axis
+    """
+    assert n >= 0, "n must be 0 or greater"
+    for liveplot in bec._live_plots.values():
+        lp = liveplot.get(y.name)
+        if lp is None or lp.x != x.name or lp.y != y.name:
+            continue
+
+        # pick out only the traces that contain plot data
+        # skipping the lines that show peak centers
+        lines = [
+            tr
+            for tr in lp.ax.lines
+            if len(tr._x) != 2 
+                or len(tr._y) != 2 
+                or (len(tr._x) == 2 
+                    and tr._x[0] != tr._x[1])
+        ]
+        if len(lines) > n:
+            print(f"limiting LivePlot({y.name}) to {n} traces")
+            lp.ax.lines = lines[-n:]
+            lp.ax.legend()
+            if n > 0:
+                lp.update_plot()
+        return lp
