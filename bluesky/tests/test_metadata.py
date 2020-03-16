e09d017f578c935bfbc778f5dccc72ea1eec8a00
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2020-03-16 18:22:27 -0400

    FMT: flake8

--- bluesky/tests/test_metadata.py
+++ bluesky/tests/test_metadata.py
@@ -1,8 +1,10 @@
 import bluesky
 import ophyd
 
+
 def test_blueskyversion(RE):
     assert RE.md['versions'].get('bluesky') == bluesky.__version__
+
 
 def test_ophydversion(RE):
     assert RE.md['versions'].get('ophyd') == ophyd.__version__
