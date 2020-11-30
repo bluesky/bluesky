bf7885b28ae1e9982d202a95c8ddb31d074a8652
Callum Forrester <callum.forrester@diamond.ac.uk>
2020-11-30 16:39:00 +0000

    First attempt at a Jupyter notebook progress bar manager. Uses the tqdm.notebook module internally.

--- bluesky/utils.py
+++ bluesky/utils.py
@@ -1,3 +1,4 @@
+import hashlib
 from collections import namedtuple
 import asyncio
 import os
@@ -5,7 +6,9 @@ import sys
 import signal
 import operator
 import uuid
+from dataclasses import dataclass
 from functools import reduce
+from typing import List, Any, TextIO, Optional, Callable, Union
 from weakref import ref, WeakKeyDictionary
 import types
 import inspect
@@ -14,12 +17,16 @@ import itertools
 import abc
 from collections.abc import Iterable
 import numpy as np
+from IPython.core.display import display, HTML
 from cycler import cycler
 import datetime
 from functools import wraps, partial
 import threading
 import time
+
+from ipywidgets import HBox
 from tqdm import tqdm
+from tqdm.notebook import tqdm as tqdm_nb
 from tqdm.utils import _screen_shape_wrapper, _term_move_up, _unicode
 import warnings
 
@@ -1258,10 +1265,219 @@ class ProgressBar:
                 self.fp.write(_unicode(_term_move_up() * len(self.meters)))
 
 
+@dataclass
+class TqdmPBarWrapper:
+    """
+    Wrapper around a TQDM notebook progress bar widget that can update
+    it with relevant data.
+    """
+
+    container: HBox
+
+    def draw(
+            self,
+            label: str = "",
+            meta: str = "",
+            color: str = "#97d4e8",
+            total: float = 1.0,
+            value: Optional[float] = None,
+    ) -> None:
+        """
+        Draws the progress bar or a message if there is
+        not enough information
+        :param label: The label to go on the left-hand side of the progress bar
+        :param meta: Any metadata to go on the right-hand side of the progress bar
+        :param color: The color of the progress bar as a valid HTML/CSS string
+        :param total: The maximum value of the progress bar, defaults to 1
+        :param value: The current value of the progress bar, optional
+        :return:
+        """
+
+        ltext, pbar, rtext = self.container.children
+        ltext.value = label
+        pbar.layout.display = "flex"
+        rtext.value = meta
+
+        if value is not None:
+            pbar.value = value
+            pbar.max = total or 1
+            pbar.style.bar_color = color
+        else:
+            pbar.layout.display = "none"
+
+    def display(self) -> None:
+        """
+        Displays the progress bar widget in a Jupyter notebook
+        :return: None
+        """
+
+        display(self.container)
+
+    def clear(self) -> None:
+        """
+        Removes the progress bar widget permanently
+        :return: None
+        """
+
+        self.container.close()
+
+
+class NotebookProgressBar:
+    containers: List[TqdmPBarWrapper]
+    status_objs: List[Any]
+    fp: TextIO
+    creation_time: float
+    lock: threading.RLock
+
+    def __init__(self, status_objs: List[Any]):
+        """
+        Represents status objects with Jupyter Notebook progress bars.
+
+        Parameters
+        ----------
+        status_objs : list
+            Status objects
+        """
+        self.containers = []
+        self.status_objs = []
+        self.fp = sys.stdout
+        self.creation_time = time.time()
+        self.lock = threading.RLock()
+
+        # Create a closure over self.update for each status object that
+        # implemets the 'watch' method.
+        for st in status_objs:
+            with self.lock:
+                if hasattr(st, "watch") and not st.done:
+                    pos = len(self.containers)
+                    container = TqdmPBarWrapper(
+                        tqdm_nb.status_printer(self.fp, 1, None, None)
+                    )
+                    container.display()
+                    self.containers.append(container)
+                    self.status_objs.append(st)
+                    st.watch(partial(self.update, pos))
+
+    def update(
+            self,
+            pos: Any,
+            *,
+            name: str = None,
+            current: Any = None,
+            initial: Any = None,
+            target: Any = None,
+            unit: str = "units",
+            precision: Any = None,
+            fraction: Any = None,
+            time_elapsed: float = None,
+            time_remaining: float = None,
+    ):
+        container = self.containers[pos]
+
+        if all(x is not None for x in (current, initial, target)):
+            # In this case there is enough information to draw a progress bar with
+            # current and maximum values
+            total = round(_L2norm(target, initial), precision or 3)
+            n = np.clip(round(_L2norm(current, initial), precision or 3), 0, total)
+            if time_elapsed is None:
+                time_elapsed = time.time() - self.creation_time
+            meta = tqdm_nb.format_meter(
+                n=n,
+                total=total,
+                elapsed=time_elapsed,
+                unit=unit,
+                prefix=name,
+                ncols=79,
+                bar_format="{r_bar}",
+            )
+            with self.lock:
+                container.draw(
+                    name or "",
+                    meta,
+                    color=html_color_from_name(name or ""),
+                    total=total,
+                    value=n,
+                )
+        elif self.status_objs[pos].done:
+            # In this case the operation is complete, we just
+            # display a finished progress bar
+            # and a completion message.
+            with self.lock:
+                container.draw(
+                    name or "",
+                    "[Complete.]",
+                    color=html_color_from_name(name or ""),
+                    value=1,
+                )
+        else:
+            # In this case there is not enough information to draw a progress bar so
+            # we just display a message.
+            with self.lock:
+                container.draw(name or "", "[In progress. No progress bar available.]")
+
+    def clear(self) -> None:
+        """
+        Clears all progress bars
+        :return: None
+        """
+
+        with self.lock:
+            for container in self.containers:
+                container.clear()
+        hide_leftover_progress_bars()
+
+
+def html_color_from_name(name: str) -> str:
+    """
+    Transforms a string into a unique, bright HTML color
+    (hue varies, saturation and value are fixed).
+    Transformation is pure.
+    :param name: The string to convert
+    :return: A string describing a color, valid in HTML/CSS.
+    """
+
+    hue = int(hashlib.sha1(name.encode("utf-8")).hexdigest(), 16) % 360
+    return f"hsl({hue}, 90%, 55%)"
+
+
+def hide_leftover_progress_bars() -> None:
+    """
+    Sets the style in Jupyterlab to remove leftover whitespace
+    from deleted progress bars. Workaround for:
+    https://github.com/jupyterlab/jupyterlab/issues/7354
+    :return: None
+    """
+
+    display(
+        HTML(
+            """
+        <style>
+            .p-Widget.jp-OutputPrompt.jp-OutputArea-prompt:empty {
+                  padding: 0;
+                  border: 0;
+            }
+        </style>
+    """
+        )
+    )
+
+
+# TODO: Use a protocol or an ABC
+AbstractProgressBar = Union[ProgressBar, NotebookProgressBar]
+
+
 class ProgressBarManager:
-    def __init__(self, delay_draw=0.2):
-        self.delay_draw = delay_draw
+    pbar_factory: Callable[[Any], AbstractProgressBar]
+    pbar: Optional[AbstractProgressBar]
+
+    def __init__(self,
+                 pbar_factory: Callable[[Any], AbstractProgressBar] = ProgressBar):
+        self.pbar_factory = pbar_factory
         self.pbar = None
+
+    @classmethod
+    def notebook(cls) -> 'ProgressBarManager':
+        return ProgressBarManager(NotebookProgressBar)
 
     def __call__(self, status_objs_or_none):
         if status_objs_or_none is not None:
@@ -1269,8 +1485,7 @@ class ProgressBarManager:
             if self.pbar is not None:
                 warnings.warn("Previous ProgressBar never competed.")
                 self.pbar.clear()
-            self.pbar = ProgressBar(status_objs_or_none,
-                                    delay_draw=self.delay_draw)
+            self.pbar = self.pbar_factory(status_objs_or_none)
         else:
             # Clean up an old one.
             if self.pbar is None:
