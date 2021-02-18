d72b0f101e6fcc1ebe24f05766f507c4e523aa7f
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2021-02-18 13:10:56 -0500

    FIX: redirecting of logging to a file on disk

--- bluesky/commandline/zmq_proxy.py
+++ bluesky/commandline/zmq_proxy.py
@@ -46,20 +46,22 @@ def main():
                         help=("Show 'start' and 'stop' documents. "
                               "(Use -vvv to show all documents.)"))
     parser.add_argument('--logfile', type=str,
-                        help="Write logfile")
+                        help="Redirect logging output to a file on disk.")
     args = parser.parse_args()
     in_port = args.in_port[0]
     out_port = args.out_port[0]
 
     if args.verbose:
         from bluesky.log import config_bluesky_logging
-        if args.verbose <= 2:  # called with -v or -vv
-            config_bluesky_logging(level='INFO')
-        else:  # called with -vvv
-            config_bluesky_logging(level='DEBUG')
+        # "INFO" if called with '-v' or '-vv', "DEBUG" if called with '-vvv'
+        level = "INFO" if args.verbose <= 2 else 'DEBUG'
+        if args.logfile:
+            config_bluesky_logging(level=level, file=args.logfile)
+        else:
+            config_bluesky_logging(level=level)
         # Set daemon to kill all threads upon IPython exit
         threading.Thread(target=start_dispatcher,
-                         args=('localhost', out_port, args.logfile),
+                         args=('localhost', out_port, None),
                          daemon=True).start()
 
     print("Connecting...")
