32bcfc53d0a9553f4b5c5b2af342409057464afc
Kyle Sunden <git@ksunden.space>
2021-04-16 14:13:22 -0500

    Remove unused import

--- bluesky/abc.py
+++ bluesky/abc.py
@@ -3,7 +3,7 @@ try:
 except ImportError:
     from typing_extensions import Protocol, runtime_checkable
 
-from typing import Dict, Any, TypeVar, Optional, Callable, Generator
+from typing import Dict, Any, Optional, Callable, Generator
 
 
 Configuration = Dict[str, Dict[str, Any]]
