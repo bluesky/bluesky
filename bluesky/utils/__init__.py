d0b3a7be937c71272e06d251f0a52720a4ba1374
Russ Berg <russ.berg@lightsource.ca>
2021-12-16 14:13:59 -0600

    added back install-qt_kicker with a different name so that it can be used by a Qt process outside the process that created the RunEngine

--- bluesky/utils/__init__.py
+++ bluesky/utils/__init__.py
@@ -928,6 +928,56 @@ def install_qt_kicker(loop=None, update_rate=0.03):
                   "has no effect. Please remove your use of it. It may be "
                   "removed in a future release of bluesky.")
 
+def install_remote_qt_kicker(loop=None, update_rate=0.03):
+    """Install a periodic callback to integrate Qt and asyncio event loops.
+
+    If a version of the Qt bindings are not already imported, this function
+    will do nothing.
+
+    It is safe to call this function multiple times.
+
+    This is used when a Qt event loop is required and the process that needs
+    it is different that the process that created the RunEngine,
+    (see docstring for deprecated install_qt_kicker above)
+
+    Parameters
+    ----------
+    loop : event loop, optional
+    update_rate : number
+        Seconds between periodic updates. Default is 0.03.
+    """
+    if loop is None:
+        loop = asyncio.get_event_loop()
+    global _QT_KICKER_INSTALLED
+    if loop in _QT_KICKER_INSTALLED:
+        return
+    if not any(p in sys.modules for p in ['PyQt4', 'pyside', 'PyQt5']):
+        return
+
+    import matplotlib.backends.backend_qt
+    from matplotlib.backends.backend_qt import _create_qApp
+    from matplotlib._pylab_helpers import Gcf
+
+    _create_qApp()
+    qApp = matplotlib.backends.backend_qt.qApp
+
+    try:
+        _draw_all = Gcf.draw_all  # mpl version >= 1.5
+    except AttributeError:
+        # slower, but backward-compatible
+        def _draw_all():
+            for f_mgr in Gcf.get_all_fig_managers():
+                f_mgr.canvas.draw_idle()
+
+    def _qt_kicker():
+        # The RunEngine Event Loop interferes with the qt event loop. Here we
+        # kick it to keep it going.
+        _draw_all()
+
+        qApp.processEvents()
+        loop.call_later(update_rate, _qt_kicker)
+
+    _QT_KICKER_INSTALLED[loop] = loop.call_soon(_qt_kicker)
 
 def install_nb_kicker(loop=None, update_rate=0.03):
     """
