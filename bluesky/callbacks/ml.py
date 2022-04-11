0fdd3ee10dd5a3bef83969e6d006a6959bee5b2b
maffettone <phillip.maffettone@gmail.com>
2022-04-11 13:26:20 -0400

    Framework callback and unit tests

---
+++ bluesky/callbacks/ml.py
@@ -0,0 +1,99 @@
+"""Callbacks for AI/ML agents"""
+
+from .core import CallbackBase, make_class_safe
+from warnings import warn
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+@make_class_safe(logger=logger)
+class AgentCallback(CallbackBase):
+    def __init__(
+        self,
+        *agents,
+        independent_key: str,
+        dependent_key: str,
+        report_on_event: bool = True,
+        report_on_stop: bool = True,
+        stream_name: str = "primary",
+    ):
+        """
+        Callback for telling AI/ML agents about data, and generating reports.
+        This callback does not support fully adaptive planning;
+        however, can be used for streaming analysis, reports, or alerts.
+
+
+        Parameters
+        ----------
+        agents :
+        independent_key : str
+        dependent_key : str
+        report_on_event : bool
+        report_on_stop : bool
+        stream_name : str
+
+        Examples
+        --------
+        >>> class Agent:
+        >>>     self.positions = []
+        >>>     self.intensities = []
+        >>>     def tell(self, position, intensity):
+        >>>         self.positions.append(position)
+        >>>         self.intensities.append(intensity)
+        >>>     def report(self):
+        >>>         if self.intensities[-1] < 0: print(f"WARNING, negative intensity at {self.positions[-1]}.")
+        >>> agent = Agent()
+        >>> RE.subscribe(AgentCallback(agent, independent_key="position", dependent_key="intensity"))
+        """
+
+        super().__init__()
+        self.agents = list()
+        for agent in agents:
+            if self.agent_validation(agent):
+                self.agents.append(agent)
+            else:
+                warn(f"Agent did not pass validation and will not be included in the callback:\n{agent}")
+
+        self.independent_key = independent_key
+        self.dependent_key = dependent_key
+        self.report_on_event = report_on_event
+        self.report_on_stop = report_on_stop
+        self.stream = stream_name
+        self._descriptors = {}
+
+    @staticmethod
+    def agent_validation(agent):
+        passing = True
+        tell = getattr(agent, "tell", None)
+        if not callable(tell):
+            passing = False
+            warn("Agent missing callable tell method.")
+        report = getattr(agent, "report", None)
+        if not callable(report):
+            passing = False
+            warn("Agent missing callable report method.")
+        return passing
+
+    def descriptor(self, doc):
+        """Track descriptor documents to only send correct stream to agents"""
+        self._descriptors[doc["uid"]] = doc
+
+    def event(self, doc):
+        descriptor = self._descriptors[doc["descriptor"]]
+        if descriptor.get("name") != self.stream:
+            return
+
+        x = doc["data"][self.independent_key]
+        y = doc["data"][self.dependent_key]
+        for agent in self.agents:
+            agent.tell(x, y)
+
+        if self.report_on_event:
+            for agent in self.agents:
+                agent.report()
+
+    def stop(self, doc):
+        if self.report_on_stop:
+            for agent in self.agents:
+                agent.report()
---
+++ bluesky/tests/test_cb_ml.py
@@ -0,0 +1,121 @@
+import pytest
+from bluesky.callbacks.ml import AgentCallback
+import pandas as pd
+from bluesky.plans import scan
+
+
+class DummyAgent:
+    """Simple agent reports a statistical summary of data"""
+
+    def __init__(self, verbose=True):
+        self.independents = []
+        self.dependents = []
+        self.last_summary = None
+        self.verbose = verbose
+        self.n_reports = 0
+
+    def tell(self, x, y):
+        self.independents.append(x)
+        self.dependents.append(y)
+        self.last_summary = pd.Series(self.dependents).describe()
+
+    def ask(self):
+        raise NotImplementedError
+
+    def report(self):
+        self.n_reports += 1
+        if self.verbose:
+            print(self.last_summary)
+        return self.last_summary
+
+
+def test_bad_agent():
+    class BadAgent:
+        def tells(self, x, y):
+            pass
+
+        def reporting(self):
+            pass
+
+    class WorseAgent:
+        def __init__(self):
+            self.tell = None
+            self.report = 4
+
+    with pytest.warns(UserWarning):
+        # Wrong names for function calls
+        cb = AgentCallback(BadAgent(), independent_key="", dependent_key="")
+    with pytest.warns(UserWarning):
+        # Not callable attributes
+        cb = AgentCallback(WorseAgent(), independent_key="", dependent_key="")
+
+
+def test_simple(RE, hw):
+    cb = AgentCallback(DummyAgent(), independent_key="motor", dependent_key="det")
+    RE.subscribe(cb)
+    RE(scan([hw.det], hw.motor, 1, 3, num=3))
+
+
+def test_multi_agent(RE, hw):
+    cb = AgentCallback(
+        DummyAgent(verbose=False), DummyAgent(verbose=False), independent_key="motor", dependent_key="det"
+    )
+    assert cb.agents[0] is not cb.agents[1]
+    RE.subscribe(cb)
+    RE(scan([hw.det], hw.motor, 1, 3, num=3))
+    assert (cb.agents[0].last_summary == cb.agents[1].last_summary).all()
+
+
+def test_closure(RE, hw):
+    agent = DummyAgent(verbose=False)
+    cb = AgentCallback(agent, independent_key="motor", dependent_key="det")
+    RE.subscribe(cb)
+    RE(scan([hw.det], hw.motor, 1, 3, num=3))
+    assert agent.last_summary.astype(bool).all()
+
+
+def test_report_on_event(RE, hw):
+    agent = DummyAgent(verbose=False)
+    cb = AgentCallback(agent, independent_key="motor", dependent_key="det", report_on_stop=False)
+    RE.subscribe(cb)
+    RE(scan([hw.det], hw.motor, 1, 3, num=3))
+    assert agent.n_reports == 3
+
+
+def test_report_on_stop(RE, hw):
+    agent = DummyAgent(verbose=False)
+    cb = AgentCallback(agent, independent_key="motor", dependent_key="det", report_on_event=False)
+    RE.subscribe(cb)
+    RE(scan([hw.det], hw.motor, 1, 3, num=3))
+    assert agent.n_reports == 1
+
+
+def test_no_reports(RE, hw):
+    agent = DummyAgent(verbose=False)
+    cb = AgentCallback(
+        agent, independent_key="motor", dependent_key="det", report_on_event=False, report_on_stop=False
+    )
+    RE.subscribe(cb)
+    RE(scan([hw.det], hw.motor, 1, 3, num=3))
+    assert agent.n_reports == 0
+    assert len(agent.dependents) > 0
+
+
+def test_stream_name(RE, hw):
+    agent = DummyAgent(verbose=True)
+    cb = AgentCallback(agent, independent_key="motor", dependent_key="det", stream_name="background")
+    RE.subscribe(cb)
+    RE(scan([hw.det], hw.motor, 1, 3, num=3))
+    assert agent.last_summary is None
+
+
+def test_class_safety(RE, hw):
+    class ErrorAgent:
+        def tell(self, x, y):
+            pass
+
+        def report(self):
+            raise RuntimeError
+
+    RE.subscribe(AgentCallback(ErrorAgent(), independent_key="motor", dependent_key="det"))
+    RE(scan([hw.det], hw.motor, 1, 3, num=3))
