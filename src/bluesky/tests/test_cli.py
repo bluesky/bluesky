7f2351e85b0c351cdc37999a362210606271649a
Jakub Wlodek <jwlodek@bnl.gov>
2024-04-16 09:20:42 -0400

    Linter doesn't want newline at end of file

--- src/bluesky/tests/test_cli.py
+++ src/bluesky/tests/test_cli.py
@@ -7,4 +7,3 @@ from bluesky import __version__
 def test_cli_version():
     cmd = [sys.executable, "-m", "bluesky", "--version"]
     assert subprocess.check_output(cmd).decode().strip() == __version__
-
