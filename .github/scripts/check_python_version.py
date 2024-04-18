09afec3123af7ba3572f58c874721af3e0f88d41
maffettone <pmaffetto@bnl.gov>
2024-04-18 13:41:27 -0700

    BLD: github automations to check for outdated python

---
+++ .github/scripts/check_python_version.py
@@ -0,0 +1,72 @@
+from collections import defaultdict
+from datetime import datetime, timedelta
+
+import requests
+from packaging.version import Version
+
+# Define the release dates and core packages
+py_releases = {
+    "3.8": "Oct 14, 2019",
+    "3.9": "Oct 5, 2020",
+    "3.10": "Oct 4, 2021",
+    "3.11": "Oct 24, 2022",
+    "3.12": "Oct 2, 2023",
+}
+
+plus36 = timedelta(days=int(365 * 3))
+plus24 = timedelta(days=int(365 * 2))
+delta6month = timedelta(days=int(365 // 2))
+
+# Calculate cutoff date
+now = datetime.now()
+cutoff = now - delta6month
+
+
+def get_release_dates(package, support_time=plus24):
+    """Fetch release and drop dates for package versions."""
+    releases = {}
+    print(f"Querying pypi.org for {package} versions...", end="", flush=True)
+    response = requests.get(
+        f"https://pypi.org/simple/{package}", headers={"Accept": "application/vnd.pypi.simple.v1+json"}
+    ).json()
+    print("OK")
+    file_date = defaultdict(list)
+    for f in response["files"]:
+        ver = f["filename"].split("-")[1]
+        version = Version(ver)
+        if version.is_prerelease or version.micro != 0:
+            continue
+        release_date = None
+        for format in ["%Y-%m-%dT%H:%M:%S.%fZ", "%Y-%m-%dT%H:%M:%SZ"]:
+            release_date = datetime.strptime(f["upload-time"], format)
+        if release_date:
+            file_date[version].append(release_date)
+    release_date = {v: min(file_date[v]) for v in file_date}
+    for ver, release_date in sorted(release_date.items()):
+        drop_date = release_date + support_time
+        if drop_date >= cutoff:
+            releases[ver] = {"release_date": release_date, "drop_date": drop_date}
+    return releases
+
+
+# Gather release data for Python and packages
+package_releases = {
+    "python": {
+        version: {
+            "release_date": datetime.strptime(release_date, "%b %d, %Y"),
+            "drop_date": datetime.strptime(release_date, "%b %d, %Y") + plus36,
+        }
+        for version, release_date in py_releases.items()
+    }
+}
+
+# Check if any versions are outdated
+output_body = ""
+for package, versions in package_releases.items():
+    for version, dates in versions.items():
+        if dates["drop_date"] < now:
+            output_body += f"{package} {version} is outdated as of {dates['drop_date'].strftime('%Y-%m-%d')}\n"
+if output_body:
+    output_body = f"Outdated Python versions. Consider addressing the following:\n{output_body}"
+
+print(output_body)
