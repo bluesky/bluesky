d91912dc3a16a0567329ec2f86cac07f82d30ca7
David Perl <david.perl@diamond.ac.uk>
2024-06-18 11:36:16 +0100

    (#1682) use RE fixture in test

--- src/bluesky/tests/test_tracing.py
+++ src/bluesky/tests/test_tracing.py
@@ -3,7 +3,6 @@ from typing import Generator
 from opentelemetry.trace import get_current_span
 
 from bluesky.plan_stubs import sleep
-from bluesky.run_engine import RunEngine
 from bluesky.tracing import trace_plan, tracer
 
 
@@ -16,11 +15,10 @@ def test_trace_plan_wrapper_gives_back_generator():
     assert isinstance(_test_plan(), Generator)
 
 
-def test_trace_plan_wrapper_opens_span_but_doesnt_do_anything_without_trace_provider():
+def test_trace_plan_wrapper_opens_span_but_doesnt_do_anything_without_trace_provider(RE):
     @trace_plan(tracer, "test_plan_2")
     def test_plan_2():
         yield from sleep(0)
         assert not get_current_span().is_recording()
 
-    RE = RunEngine()
     RE(test_plan_2())
