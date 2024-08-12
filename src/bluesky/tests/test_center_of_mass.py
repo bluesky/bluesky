65fe86125520f167a07e9c3673f64cf86ab2fdb7
Pete R Jemian <prjemian@gmail.com>
2024-08-12 09:19:11 -0500

    TST #1541

--- src/bluesky/tests/test_center_of_mass.py
+++ src/bluesky/tests/test_center_of_mass.py
@@ -1,18 +1,30 @@
 import numpy as np
-from scipy import ndimage
+import pytest
 
 from bluesky.callbacks.fitting import center_of_mass
 
 
-def test_center_of_mass():
-    assert True
-
-    arr = np.array(([0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 1, 1, 0]))
-    result = ndimage.measurements.center_of_mass(arr)
-    assert result == (2.0, 1.5), f"{result=}"
-
-    # Calculation of multiple objects in an image
-    b_arr = np.array(([0, 1, 1, 0], [0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 1, 1], [0, 0, 1, 1]))
-    lbl = ndimage.label(b_arr)[0]
-    result = ndimage.measurements.center_of_mass(b_arr, lbl, [1, 2])
-    assert result == [(0.33333333333333331, 1.3333333333333333), (3.5, 2.5)]
+@pytest.mark.parametrize(
+    "arr, labels, index, expected",
+    [
+        # example from the source code
+        [[[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 1, 1, 0]], None, None, (2.0, 1.5)],
+        # signal is all zero, does not raise ZeroDivisionError or other
+        [[[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], None, None, (0.0, 0.0)],
+        # multiple objects in an image
+        # # FIXME: TypeError: Field elements must be 2- or 3-tuples, got '1'
+        # [
+        #     [[0, 1, 1, 0], [0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 1, 1], [0, 0, 1, 1]],
+        #     [[0, 1, 1, 0], [0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 2, 2], [0, 0, 2, 2]],
+        #     [1, 2],
+        #     [(1.0 / 3, 4.0 / 3), (3.5, 2.5)],
+        # ],
+        # 1-D
+        [[0, 1, 1, 1, 0], None, None, (2.0,)],
+    ],
+)
+def test_center_of_mass(arr, labels, index, expected):
+    if labels is not None:
+        labels = np.array(labels)
+    result = center_of_mass(np.array(arr), labels=labels, index=index)
+    assert result == expected, f"{result=}"
