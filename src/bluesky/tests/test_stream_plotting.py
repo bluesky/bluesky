be58c1585aabcb01197e2d6f42538ebc0f649703
Eugene M <ymatviych@bnl.gov>
2024-09-30 11:05:18 -0400

    TST: Enable BLUESKY_DEBUG_CALLBACKS in tests

--- src/bluesky/tests/test_stream_plotting.py
+++ src/bluesky/tests/test_stream_plotting.py
@@ -2,6 +2,7 @@ import os
 
 import matplotlib
 import matplotlib.pyplot as plt
+import pytest
 from ophyd_async.sim.demo import PatternDetector
 
 import bluesky.plans as bp
@@ -13,17 +14,27 @@ if not os.getenv("GITHUB_ACTIONS"):
     plt.ion()
 
 
+@pytest.fixture(autouse=True, params=["1", None])
+def _strict_debug(monkeypatch, request):
+    if request.param is not None:
+        monkeypatch.setenv("BLUESKY_DEBUG_CALLBACKS", request.param)
+    else:
+        monkeypatch.delenv("BLUESKY_DEBUG_CALLBACKS", raising=False)
+
+
 def test_hdf5_plotting_1d(RE, tmp_path):
     cl = CollectLiveStream()
     pl = LiveStreamPlot(cl, data_key="PATTERN1-sum")
+    RE.subscribe(cl)
     RE.subscribe(pl)
     det = PatternDetector(name="PATTERN1-sum", path=tmp_path)
-    RE(bp.count([det], num=15), cl)
+    RE(bp.count([det], num=15))
 
 
 def test_hdf5_plotting_2d(RE, tmp_path):
     cl = CollectLiveStream()
     pl = LiveStreamPlot(cl, data_key="PATTERN1")
     RE.subscribe(pl)
+    RE.subscribe(cl)
     det = PatternDetector(name="PATTERN1", path=tmp_path)
-    RE(bp.count([det], num=15), cl)
+    RE(bp.count([det], num=15))
