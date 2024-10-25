3c236f4328c48ea925097fd66a8314448c7c1b70
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2024-10-25 20:38:21 +0100

    Skip tests which use os.kill(pid, signal.SIGINT) on windows

--- src/bluesky/tests/__init__.py
+++ src/bluesky/tests/__init__.py
@@ -1,3 +1,4 @@
+import os
 from types import ModuleType
 from typing import Optional
 
@@ -17,3 +18,7 @@ except ImportError as ie:
 
 # define a skip condition based on if ophyd is available or not
 requires_ophyd = pytest.mark.skipif(ophyd is None, reason=reason)
+
+uses_os_kill_sigint = pytest.mark.skipif(
+    os.name == "nt", reason="os.kill on windows ignores signal argument and kills the entire process."
+)
