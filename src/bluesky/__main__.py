86d22112e75db5e23157c5eb6e1f1237e1e6cbda
Thomas A Caswell <tcaswell@bnl.gov>
2024-12-18 14:14:05 -0500

    MNT: make typing in __main__.py work on py39

--- src/bluesky/__main__.py
+++ src/bluesky/__main__.py
@@ -1,15 +1,17 @@
 """Interface for ``python -m bluesky``."""
-
+from typing import Union
 from argparse import ArgumentParser
 from collections.abc import Sequence
+
 
 from . import __version__
 
 __all__ = ["main"]
 
 
-def main(args: Sequence[str] | None = None) -> None:
+def main(args: Union[Sequence[str], None] = None) -> None:
     """Argument parser for the CLI."""
+
     parser = ArgumentParser()
     parser.add_argument(
         "-v",
