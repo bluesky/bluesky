6bd3f04ace26d6505f4ba4851e9667b214cc4c6e
Pete R Jemian <prjemian@users.noreply.github.com>
2024-12-29 10:44:44 -0600

    Update src/bluesky/tests/test_center_of_mass.py
    Co-authored-by: Thomas A Caswell <tcaswell@bnl.gov>

--- src/bluesky/tests/test_center_of_mass.py
+++ src/bluesky/tests/test_center_of_mass.py
@@ -5,26 +5,15 @@ from bluesky.callbacks.fitting import center_of_mass
 
 
 @pytest.mark.parametrize(
-    "arr, labels, index, expected",
+    "arr, expected",
     [
         # example from the source code
-        [[[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 1, 1, 0]], None, None, (2.0, 1.5)],
+        [[[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 1, 1, 0]], (2.0, 1.5)],
         # signal is all zero, does not raise ZeroDivisionError or other
-        [[[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], None, None, None],
-        # multiple objects in an image
-        # # FIXME: TypeError: Field elements must be 2- or 3-tuples, got '1'
-        # [
-        #     [[0, 1, 1, 0], [0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 1, 1], [0, 0, 1, 1]],
-        #     [[0, 1, 1, 0], [0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 2, 2], [0, 0, 2, 2]],
-        #     [1, 2],
-        #     [(1.0 / 3, 4.0 / 3), (3.5, 2.5)],
-        # ],
-        # 1-D
-        [[0, 1, 1, 1, 0], None, None, (2.0,)],
+        [[[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],  None],
+        [[0, 1, 1, 1, 0], (2.0,)],
     ],
 )
 def test_center_of_mass(arr, labels, index, expected):
-    if labels is not None:
-        labels = np.array(labels)
-    result = center_of_mass(np.array(arr), labels=labels, index=index)
+    result = center_of_mass(np.array(arr))
     assert result == expected, f"{result=}"
