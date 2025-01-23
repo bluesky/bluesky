b9b50adfdb2d32e706cca152e8f7ca2028e30d15
thomashopkins32 <thopkins1@bnl.gov>
2025-01-23 11:15:16 -0500

    Remove TypeGuard since it does not exist in python==3.9

--- src/bluesky/bundlers.py
+++ src/bluesky/bundlers.py
@@ -4,7 +4,7 @@ import time as ttime
 from collections import defaultdict, deque
 from collections.abc import Iterable
 from logging import LoggerAdapter
-from typing import Any, Callable, Literal, Optional, TypeGuard, Union, cast
+from typing import Any, Callable, Literal, Optional, Union, cast
 
 from event_model import (
     ComposeDescriptorBundle,
@@ -58,7 +58,7 @@ ExternalAssetDoc = Union[Datum, Resource, StreamDatum, StreamResource]
 
 def _describe_collect_dict_is_valid(
     describe_collect_dict: Union[Any, dict[str, Any]],
-) -> TypeGuard[dict[str, DataKey]]:
+) -> bool:  # TODO: change to TypeGuard[dict[str, DataKey]] after python 3.9
     """
     Check if the describe_collect dictionary contains valid DataKeys.
     """
@@ -683,25 +683,31 @@ class RunBundler:
         If the `message_stream_name` is None then return the `describe_collect_dict.items()`.
         """
 
-        def _has_nested_structure(
+        def _has_nested_structure_and_valid_substructure(
             describe_collect_dict: Union[Any, dict[str, Any]],
-        ) -> TypeGuard[dict[str, dict[str, DataKey]]]:
+        ) -> bool:  # TODO: change to TypeGuard[dict[str, dict[str, DataKey]]] after python 3.9
+            if not isinstance(describe_collect_dict, dict):
+                return False
             return all(_describe_collect_dict_is_valid(v) for v in describe_collect_dict.values())
 
         if describe_collect_dict:
             if _describe_collect_dict_is_valid(describe_collect_dict):
-                return [(message_stream_name or "primary", describe_collect_dict)]
+                # TODO: remove cast after python 3.9 is no longer supported
+                flat_describe_collect_dict = cast(dict[str, DataKey], describe_collect_dict)
+                return [(message_stream_name or "primary", flat_describe_collect_dict)]
             # Validate that all of the values nested values are DataKeys
-            elif _has_nested_structure(describe_collect_dict):
+            elif _has_nested_structure_and_valid_substructure(describe_collect_dict):
                 # We have Dict[str, Dict[str, DataKey]] so return its items
-                if message_stream_name and list(describe_collect_dict) != [message_stream_name]:
+                # TODO: remove cast after python 3.9 is no longer supported
+                nested_describe_collect_dict = cast(dict[str, dict[str, DataKey]], describe_collect_dict)
+                if message_stream_name and list(nested_describe_collect_dict) != [message_stream_name]:
                     # The collect contained a name and describe_collect returned a Dict[str, Dict[str, DataKey]],
                     # this is only acceptable if the only key in the parent dict is message_stream_name
 
                     raise RuntimeError(
                         f"Expected a single stream {message_stream_name!r}, got {describe_collect_dict}"
                     )
-                return list(describe_collect_dict.items())
+                return list(nested_describe_collect_dict.items())
             else:
                 raise RuntimeError(
                     f"Invalid describe_collect return: {describe_collect_dict} when collect "
