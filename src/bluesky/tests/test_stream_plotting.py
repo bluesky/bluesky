29674a6adf1c19928a2768c4931ddc3fdd89aa99
Eugene M <ymatviych@bnl.gov>
2025-03-10 09:40:07 -0400

    TST: fix tests to use ophyd_async.sim devices

--- src/bluesky/tests/test_stream_plotting.py
+++ src/bluesky/tests/test_stream_plotting.py
@@ -1,9 +1,11 @@
 import os
+from tempfile import mkdtemp
 
 import matplotlib
 import matplotlib.pyplot as plt
 import pytest
-from ophyd_async.sim.demo import PatternDetector
+from ophyd_async import sim
+from ophyd_async.core import StaticPathProvider, UUIDFilenameProvider, init_devices
 
 import bluesky.plans as bp
 from bluesky.callbacks.core import CollectLiveStream
@@ -22,19 +24,28 @@ def _strict_debug(monkeypatch, request):
         monkeypatch.delenv("BLUESKY_DEBUG_CALLBACKS", raising=False)
 
 
-def test_hdf5_plotting_1d(RE, tmp_path):
+def test_hdf5_plotting_1d(RE):
+    pattern_generator = sim.PatternGenerator()
+    path_provider = StaticPathProvider(UUIDFilenameProvider(), mkdtemp())
+    with init_devices():
+        bdet = sim.SimBlobDetector(path_provider, pattern_generator)
+
     cl = CollectLiveStream()
-    pl = LiveStreamPlot(cl, data_key="PATTERN1-sum")
+    pl = LiveStreamPlot(cl, data_key="bdet-sum")
     RE.subscribe(cl)
     RE.subscribe(pl)
-    det = PatternDetector(name="PATTERN1-sum", path=tmp_path)
-    RE(bp.count([det], num=15))
+
+    RE(bp.count([bdet], num=5))
 
 
-def test_hdf5_plotting_2d(RE, tmp_path):
+def test_hdf5_plotting_2d(RE):
+    pattern_generator = sim.PatternGenerator()
+    path_provider = StaticPathProvider(UUIDFilenameProvider(), mkdtemp())
+    with init_devices():
+        bdet = sim.SimBlobDetector(path_provider, pattern_generator)
+
     cl = CollectLiveStream()
     pl = LiveStreamPlot(cl, data_key="PATTERN1")
     RE.subscribe(pl)
     RE.subscribe(cl)
-    det = PatternDetector(name="PATTERN1", path=tmp_path)
-    RE(bp.count([det], num=15))
+    RE(bp.count([bdet], num=5))
