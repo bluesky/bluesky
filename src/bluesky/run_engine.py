0b88e231637123726196c138ceedf5897de56d63
Thomas Ferreira de Lima <github@tlima.me>
2025-09-29 21:46:16 -0400

    BUG: RunEngine._wait fails when waiting for a status times out

--- src/bluesky/run_engine.py
+++ src/bluesky/run_engine.py
@@ -2334,7 +2334,7 @@ class RunEngine:
                 await status_task
             except WaitForTimeoutError:
                 # We might wait to call wait again, so put the futures and status objects back in
-                self._groups[group] = futs
+                self._groups[group].update(futs)
                 self._status_objs[group] = status_objs
                 if error_on_timeout:
                     raise
