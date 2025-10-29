8489d26174557b80139c96ca363e896e854f5839
Eugene M <ymatviych@bnl.gov>
2025-10-29 10:56:14 -0400

    ENH: use the top-level import for TW

--- src/bluesky/callbacks/tiled_writer.py
+++ src/bluesky/callbacks/tiled_writer.py
@@ -1,10 +1,11 @@
 import warnings
 
-from bluesky_tiled_plugins.tiled_writer import RunNormalizer, TiledWriter
+from bluesky_tiled_plugins import TiledWriter
+from bluesky_tiled_plugins.tiled_writer import RunNormalizer
 
 warnings.warn(
     "The bluesky.callbacks.tiled_writer module is deprecated. "
-    "Please use bluesky_tiled_plugins.tiled_writer instead.",
+    "Please import TiledWriter from bluesky_tiled_plugins instead.",
     DeprecationWarning,
     stacklevel=2,
 )
