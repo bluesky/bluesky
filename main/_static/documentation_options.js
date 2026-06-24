4914e2eff178e1d5466f78fa2b644d534ca1707d
danielballan <danielballan@users.noreply.github.com>
2026-06-24 16:00:40 +0000

    deploy: 8b8b6dc8f53e7f9a2757ef487ad308cbb9861422

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.15.2.dev20+gd276e2bf8',
+    VERSION: '1.15.2.dev24+g8b8b6dc8f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
