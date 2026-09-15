33dabd611b9142f8ffe551ca74f7335c7047e8e0
Thomas A Caswell <tcaswell@bnl.gov>
2026-09-14 22:26:39 -0400

    FIX: drop a noqa by setting a warning stacklevel
    Set to 4 to get out of RE code

--- src/bluesky/utils/__init__.py
+++ src/bluesky/utils/__init__.py
@@ -2007,7 +2007,7 @@ In the future the passing of Msg.args and Msg.kwargs down to hardware from
 Msg("{msg.command}") may be deprecated. If you have a use case for these,
 we would like to know about it, so please open an issue at
 https://github.com/bluesky/bluesky/issues"""
-        warnings.warn(error_msg)  # noqa: B028
+        warnings.warn(error_msg, stacklevel=4)
 
 
 def maybe_update_hints(hints: dict[str, Hints], obj):
